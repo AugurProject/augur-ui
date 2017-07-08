8da9bfd8de37c049a9ea01470e7a7f9c6a9260ba
JohnAllen <johnjalleniii@gmail.com>
2017-07-08 16:37:01 +0200

    Category-title is now stateless

--- src/modules/categories/components/category-title.jsx
+++ src/modules/categories/components/category-title.jsx
@@ -1,30 +1,11 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import ReactTooltip from 'react-tooltip';
-import classNames from 'classnames';
+import React from 'react';
 
-
-export default class CategoryTitle extends Component {
-  static propTypes = {
-    category: PropTypes.string
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const p = this.props;
-
-    return (
-      <div className="category-title">
-        <span ref={(categoryName) => {
-          this.categoryName = categoryName;
-        }}>
-          {p.category.toUpperCase()}
-        </span>
-      </div>
-    );
-  }
+const CategoryTitle = (p) => {
+  return (
+    <div className="category-title">
+      {this.props.category.toUpperCase()}
+    </div>
+  );
 }
+
+export default CategoryTitle;
