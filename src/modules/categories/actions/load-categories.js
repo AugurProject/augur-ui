1b93b75e4a7680e19d9b4c44d82b6338fb8c8fbf
JohnAllen <johnjalleniii@gmail.com>
2017-07-09 10:39:57 +0200

    Revert augur.js category changes until it has changed

--- src/modules/categories/actions/load-categories.js
+++ src/modules/categories/actions/load-categories.js
@@ -4,8 +4,8 @@ import logError from 'utils/log-error';
 
 export const loadCategories = (branchID, callback = logError) => (dispatch) => {
   let firstChunkLoaded;
-  augur.categories.getCategoriesInfoChunked(branchID, 0, null, null, (categoriesInfoChunk) => {
-    console.log('categories info chunk:', categoriesInfoChunk);
+  augur.topics.getTopicsInfoChunked(branchID, 0, null, null, (categoriesInfoChunk) => {
+    console.log('categories/topics info chunk:', categoriesInfoChunk);
     if (!firstChunkLoaded) {
       firstChunkLoaded = true;
       console.log('first chunk, clearing categories...');
