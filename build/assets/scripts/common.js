5aa477bafad99a177f763965e9c9ec15871b058c
Jack Peterson <jack@tinybike.net>
2017-10-02 18:00:39 -0700

    Removed loadRegisterBlockNumber and registerBlockNumber references

--- build/assets/scripts/common.js
+++ build/assets/scripts/common.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"markets","1":"topics","2":"transactions","3":"account","4":"market","5":"portfolio","6":"create-market","7":"auth","8":"main","9":"assets/scripts/vendor","10":"assets/styles/styles"}[chunkId]||chunkId) + "." + {"0":"0aaddef2092c4eca5752","1":"cbd97f46b988355df77c","2":"49f137741496305bc1b4","3":"bae9bc37f2396841bfb7","4":"cca05dd8d9d62cd8398b","5":"b37cbf2786a060250d00","6":"aec53348d5058d060385","7":"48d4ebd3856b7009f837","8":"a0dca14824760592e9dd","9":"4e7de5814fa36d718fe7","10":"b9cac451eb5112b01510"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"markets","1":"topics","2":"transactions","3":"account","4":"market","5":"portfolio","6":"create-market","7":"auth","8":"main","9":"assets/scripts/vendor","10":"assets/styles/styles"}[chunkId]||chunkId) + "." + {"0":"475517c486819b2be796","1":"a44ec1e3fa7d75c8599b","2":"55627af56756bda2bcd0","3":"2171f38d98e090cfe006","4":"a777678b20f90a8f1328","5":"5170b4b0a98dac821e69","6":"c00b75fee2deaa47d9fb","7":"1c4bc1be5c7666c27909","8":"b9c17366e72976ac2cca","9":"a8321eb9caf75fccf3e9","10":"17b6cc1c0c04ba7a79c2"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
---
+++ build/assets/styles/styles.17b6cc1c0c04ba7a79c2.js
@@ -0,0 +1,11 @@
+webpackJsonp([10],{
+
+/***/ 1191:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},[1191]);
+//# sourceMappingURL=styles.17b6cc1c0c04ba7a79c2.js.map
