2867b5c053d950dcc4b1f7720b07a9cd9cc82c9e
Tom Haile <bradford.haile@gmail.com>
2018-02-06 18:47:49 -0600

    force loading all outcomes for binary markets in order to get all open orders to translate.

--- src/modules/bids-asks/actions/load-account-orders.js
+++ src/modules/bids-asks/actions/load-account-orders.js
@@ -3,6 +3,8 @@ import logError from 'utils/log-error'
 import { addOpenOrderTransactions } from 'modules/transactions/actions/add-transactions'
 import { loadMarketsInfo } from 'modules/markets/actions/load-markets-info'
 import { updateOrderBook } from 'modules/bids-asks/actions/update-order-book'
+import { BINARY } from 'modules/markets/constants/market-types'
+import { translateBinaryOrders } from 'modules/bids-asks/actions/translate-binary-orders'
 
 export const loadAccountOrders = (options, callback = logError) => (dispatch, getState) => {
   const { universe, loginAccount } = getState()
@@ -11,16 +13,18 @@ export const loadAccountOrders = (options, callback = logError) => (dispatch, ge
     if (orders == null || Object.keys(orders).length === 0) return callback(null)
     const marketIDs = Object.keys(orders)
     // TODO: consolidate all the getting of maket infos for load account history
-    dispatch(loadMarketsInfo(marketIDs, () => {
-      dispatch(addOpenOrderTransactions(orders))
+    dispatch(loadMarketsInfo(marketIDs, (err, infos) => {
+      let newOrders = orders
+      newOrders = translateBinaryOrders(Object.keys(infos).filter(marketId => infos[marketId].marketType === BINARY), orders)
+      dispatch(addOpenOrderTransactions(newOrders))
       marketIDs.forEach((marketID, id) => {
-        const outcomes = Object.keys(orders[marketID])
+        const outcomes = Object.keys(newOrders[marketID])
         outcomes.forEach((outcome, id, nextOutcome) => {
-          const orderTypeLabels = Object.keys(orders[marketID][outcome])
-          orderTypeLabels.forEach(orderType => dispatch(updateOrderBook(marketID, outcome, orderType, orders[marketID][outcome][orderType])))
+          const orderTypeLabels = Object.keys(newOrders[marketID][outcome])
+          orderTypeLabels.forEach(orderType => dispatch(updateOrderBook(marketID, outcome, orderType, newOrders[marketID][outcome][orderType])))
         })
       })
-      callback(null, orders)
+      callback(null, newOrders)
     }))
   })
 }
