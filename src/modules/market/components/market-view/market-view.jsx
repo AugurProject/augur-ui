45294079b86870dcb93ed737b37bf3e11491a9ee
Stephen Sprinkle <stephen@sprinkle.io>
2018-03-01 12:38:53 -0700

    initial groundwork for update on selection + title metadata based on market description

--- src/modules/market/components/market-view/market-view.jsx
+++ src/modules/market/components/market-view/market-view.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
 
 import MarketHeader from 'modules/market/containers/market-header'
 import MarketOutcomesChart from 'modules/market/containers/market-outcomes-chart'
 import MarketOutcomeCharts from 'modules/market/containers/market-outcome-charts'
 import MarketOutcomesAndPositions from 'modules/market/containers/market-outcomes-and-positions'
 import MarketTrading from 'modules/market/containers/market-trading'
+
+import parseMarketTitle from 'modules/market/helpers/parse-market-title'
 
 import { CATEGORICAL } from 'modules/markets/constants/market-types'
 
@@ -14,20 +17,22 @@ import Styles from 'modules/market/components/market-view/market-view.styles'
 export default class MarketView extends Component {
   static propTypes = {
     marketId: PropTypes.string.isRequired,
-    marketType: PropTypes.string,
     isConnected: PropTypes.bool.isRequired,
     isMarketLoaded: PropTypes.bool.isRequired,
-    loadFullMarket: PropTypes.func.isRequired
+    loadFullMarket: PropTypes.func.isRequired,
+    marketType: PropTypes.string
   }
 
   constructor(props) {
     super(props)
 
     this.state = {
-      selectedOutcomes: props.marketType === CATEGORICAL ? [] : ['1']
+      selectedOutcomes: props.marketType === CATEGORICAL ? [] : ['1'],
+      selectedOrderProperties: {} // order properties from chart click
     }
 
     this.updateSelectedOutcomes = this.updateSelectedOutcomes.bind(this)
+    this.updateSeletedOrderProperties = this.updateSeletedOrderProperties.bind(this)
     this.clearSelectedOutcomes = this.clearSelectedOutcomes.bind(this)
   }
 
@@ -59,6 +64,15 @@ export default class MarketView extends Component {
     this.setState({ selectedOutcomes: newSelectedOutcomes })
   }
 
+  updateSeletedOrderProperties(selectedOrderProperties) {
+    this.setState({
+      selectedOrderProperties: {
+        ...this.state.selectedOrderProperties,
+        ...selectedOrderProperties
+      }
+    })
+  }
+
   clearSelectedOutcomes() {
     this.setState({ selectedOutcomes: [] })
   }
@@ -69,6 +83,9 @@ export default class MarketView extends Component {
 
     return (
       <section>
+        <Helmet>
+          <title>{parseMarketTitle(p.description)}</title>
+        </Helmet>
         <div className={Styles.Market__upper}>
           <MarketHeader
             marketId={p.marketId}
@@ -87,6 +104,7 @@ export default class MarketView extends Component {
             <MarketOutcomeCharts
               marketId={p.marketId}
               selectedOutcome={s.selectedOutcomes[0]}
+              updateSeletedOrderProperties={this.updateSeletedOrderProperties}
             />
           }
         </div>
@@ -102,6 +120,7 @@ export default class MarketView extends Component {
             <MarketTrading
               marketId={p.marketId}
               selectedOutcomes={s.selectedOutcomes}
+              selectedOrderProperties={s.selectedOrderProperties}
             />
           </div>
         </section>
