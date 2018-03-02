a7f56403f53c3df0d1200d24d9ac2bfbc3e7152b
Stephen Sprinkle <stephen@sprinkle.io>
2018-03-02 13:42:56 -0800

    error handling if failure during load of full market

--- src/modules/market/actions/load-full-market.js
+++ src/modules/market/actions/load-full-market.js
@@ -2,7 +2,7 @@ import { loadMarketsInfo } from 'modules/markets/actions/load-markets-info'
 import loadBidsAsks from 'modules/bids-asks/actions/load-bids-asks'
 import { loadAccountTrades } from 'modules/my-positions/actions/load-account-trades'
 import { loadPriceHistory } from 'modules/market/actions/load-price-history'
-import { updateMarketLoading } from 'modules/market/actions/update-market-loading'
+import { updateMarketLoading, removeMarketLoading } from 'modules/market/actions/update-market-loading'
 import logError from 'utils/log-error'
 
 import { MARKET_FULLY_LOADING, MARKET_FULLY_LOADED } from 'modules/market/constants/market-loading-states'
@@ -16,7 +16,7 @@ export const loadFullMarket = (marketId, callback = logError) => (dispatch, getS
 
   // if the basic data hasn't loaded yet, load it first
   dispatch(loadMarketsInfo([marketId], (err) => {
-    if (err) return callback(err)
+    if (err) return loadingError(dispatch, callback, err, marketId)
     dispatch(loadMarketDetails(marketId, callback))
   }))
 }
@@ -25,14 +25,19 @@ export const loadFullMarket = (marketId, callback = logError) => (dispatch, getS
 // the necessary actions to save each part in relevant state
 export const loadMarketDetails = (marketId, callback = logError) => dispatch => (
   dispatch(loadBidsAsks(marketId, (err) => {
-    if (err) return callback(err)
+    if (err) return loadingError(dispatch, callback, err, marketId)
     dispatch(loadAccountTrades({ market: marketId }, (err) => {
-      if (err) return callback(err)
+      if (err) return loadingError(dispatch, callback, err, marketId)
       dispatch(loadPriceHistory({ market: marketId }, (err) => {
-        if (err) return callback(err)
+        if (err) return loadingError(dispatch, callback, err, marketId)
         dispatch(updateMarketLoading({ [marketId]: MARKET_FULLY_LOADED }))
         callback(null)
       }))
     }))
   }))
 )
+
+function loadingError(dispatch, callback, error, marketId) {
+  dispatch(removeMarketLoading(marketId))
+  callback(error)
+}
