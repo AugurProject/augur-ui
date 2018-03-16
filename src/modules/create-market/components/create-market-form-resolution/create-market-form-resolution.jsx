7232ac68401575faa1f9953c6704ee8bcaeba89c
adrake33 <aarondrake@gmail.com>
2018-03-16 16:09:32 -0700

    Appeasing linter

--- src/modules/create-market/components/create-market-form-resolution/create-market-form-resolution.jsx
+++ src/modules/create-market/components/create-market-form-resolution/create-market-form-resolution.jsx
@@ -119,8 +119,7 @@ export default class CreateMarketResolution extends Component {
   render() {
     const p = this.props
     const validations = p.newMarket.validations[p.newMarket.currentStep]
-    const formattedDate = formatDate(new Date(Date.now()))
-    const utcLocalOffset = formattedDate.utcLocalOffset
+    const utcLocalOffset = formatDate(new Date(Date.now()))["utcLocalOffset"]
 
     const designatedReporterError = p.newMarket.designatedReporterType === DESIGNATED_REPORTER_SPECIFIC && validations.designatedReporterAddress && !!validations.designatedReporterAddress.length
 
