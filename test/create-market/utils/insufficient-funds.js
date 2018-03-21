387a108c33314acd3e5a1788ad90e1958a53cadf
adrake33 <aarondrake@gmail.com>
2018-03-21 10:50:22 -0700

    Move insufficient-funds test to utils dir and modified tests

--- test/create-market/utils/insufficient-funds.js
+++ test/create-market/utils/insufficient-funds.js
@@ -1,15 +1,15 @@
 import { describe, it } from 'mocha'
 import { assert } from 'chai'
-import insufficientFunds from 'modules/create-market/selectors/insufficient-funds'
+import insufficientFunds from 'modules/create-market/utils/insufficient-funds'
 
-describe('src/modules/create-market/selectors/insufficient-funds.js', () => {
+describe('src/modules/create-market/utils/insufficient-funds.js', () => {
   describe('when user has insufficient ETH', () => {
-    it('should.....', () => {
+    it('should output \'ETH\'', () => {
       const validityBond = 0.01
-      const gasCost = 0
-      const creationFee = 0.025
-      const designatedReportNoShowReputationBond = 0.03
-      const availableEth = 0.0001
+      const gasCost = 0.001
+      const creationFee = 0.024
+      const designatedReportNoShowReputationBond = 0.035
+      const availableEth = 0.03
       const availableRep = 10
 
       const expected = 'ETH'
@@ -19,13 +19,13 @@ describe('src/modules/create-market/selectors/insufficient-funds.js', () => {
     })
   })
   describe('when user has insufficient REP', () => {
-    it('should.....', () => {
+    it('should output \'REP\'', () => {
       const validityBond = 0.01
-      const gasCost = 0
-      const creationFee = 0.025
-      const designatedReportNoShowReputationBond = 0.03
+      const gasCost = 0.001
+      const creationFee = 0.024
+      const designatedReportNoShowReputationBond = 0.035
       const availableEth = 10
-      const availableRep = 0.0001
+      const availableRep = 0.03
 
       const expected = 'REP'
       const result = insufficientFunds(validityBond, gasCost, creationFee, designatedReportNoShowReputationBond, availableEth, availableRep)
@@ -34,13 +34,13 @@ describe('src/modules/create-market/selectors/insufficient-funds.js', () => {
     })
   })
   describe('when user has insufficient ETH and REP', () => {
-    it('should.....', () => {
+    it('should output \'ETH and REP\'', () => {
       const validityBond = 0.01
-      const gasCost = 0
-      const creationFee = 0.025
-      const designatedReportNoShowReputationBond = 0.03
-      const availableEth = 0.0001
-      const availableRep = 0.0001
+      const gasCost = 0.001
+      const creationFee = 0.024
+      const designatedReportNoShowReputationBond = 0.035
+      const availableEth = 0.03
+      const availableRep = 0.03
 
       const expected = 'ETH and REP'
       const result = insufficientFunds(validityBond, gasCost, creationFee, designatedReportNoShowReputationBond, availableEth, availableRep)
@@ -49,13 +49,13 @@ describe('src/modules/create-market/selectors/insufficient-funds.js', () => {
     })
   })
   describe('when user has sufficient funds', () => {
-    it('should.....', () => {
+    it('should output empty string', () => {
       const validityBond = 0.01
-      const gasCost = 0
-      const creationFee = 0.025
-      const designatedReportNoShowReputationBond = 0.03
-      const availableEth = 10
-      const availableRep = 10
+      const gasCost = 0.001
+      const creationFee = 0.024
+      const designatedReportNoShowReputationBond = 0.035
+      const availableEth = 0.035
+      const availableRep = 0.035
 
       const expected = ''
       const result = insufficientFunds(validityBond, gasCost, creationFee, designatedReportNoShowReputationBond, availableEth, availableRep)
