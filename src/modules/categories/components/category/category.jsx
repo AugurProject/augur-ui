brendangibson/augur

Migrate to destructured props add missing PropTypes
This allows eslint to validate prop-types.

NOTE: this doesn't cover our functional stateless components.

ref ch8405

src/modules/categories/components/category/category.jsx
@@ -15,6 +15,7 @@ import { CATEGORY_PARAM_NAME } from 'modules/filter-sort/constants/param-names'
 
 export default class Category extends Component {
   static propTypes = {
+    category: PropTypes.string,
     popularity: PropTypes.string,
   }
 
@@ -29,7 +30,8 @@ export default class Category extends Component {
   }
 
   componentWillUpdate(nextProps, nextState) {
-    if (this.props.popularity !== nextProps.popularity) this.updatePopularity(nextProps.popularity > this.props.popularity ? CATEGORY_VOLUME_INCREASED : CATEGORY_VOLUME_DECREASED)
+    const { popularity } = this.props
+    if (popularity !== nextProps.popularity) this.updatePopularity(nextProps.popularity > popularity ? CATEGORY_VOLUME_INCREASED : CATEGORY_VOLUME_DECREASED)
   }
 
   updatePopularity(popularityChange) {
@@ -37,11 +39,14 @@ export default class Category extends Component {
   }
 
   render() {
-    const p = this.props
+    const {
+      category,
+      popularity,
+    } = this.props
     const s = this.state
 
-    const isNullCategory = p.category === 'null-category' && p.popularity === 0
-    const roundedPop = WrappedBigNumber(p.popularity.toString()).integerValue(BigNumber.ROUND_HALF_EVEN)
+    const isNullCategory = category === 'null-category' && popularity === 0
+    const roundedPop = WrappedBigNumber(popularity.toString()).integerValue(BigNumber.ROUND_HALF_EVEN)
     let popString = roundedPop.toNumber() === 1 ? ' SHARE' : ' SHARES'
     if (roundedPop > 1000) {
       const thousands = roundedPop / 1000
@@ -56,7 +61,7 @@ export default class Category extends Component {
         to={{
           pathname: makePath(MARKETS),
           search: makeQuery({
-            [CATEGORY_PARAM_NAME]: p.category,
+            [CATEGORY_PARAM_NAME]: category,
           }),
         }}
         className={isNullCategory ? Styles['Category__link-hidden'] : Styles.Category__link}
@@ -66,7 +71,7 @@ export default class Category extends Component {
         >
           <div className={Styles.Category__name} >
             <span ref={(categoryName) => { this.categoryName = categoryName }}>
-              {p.category.toUpperCase()}
+              {category.toUpperCase()}
             </span>
           </div>
           <div className={Styles.Category__separator} />

src/modules/modal/components/modal-participate/modal-participate.jsx
@@ -37,9 +37,10 @@ export default class ModalParticipate extends Component {
   }
 
   triggerReview(e, ...args) {
+    const { purchaseParticipationTokens } = this.props
     e.preventDefault()
     if (this.state.isValid) {
-      this.props.purchaseParticipationTokens(this.state.quantity, true, (err, gasEstimate) => {
+      purchaseParticipationTokens(this.state.quantity, true, (err, gasEstimate) => {
         console.log('trigger review', err, gasEstimate)
         if (!err && !!gasEstimate) this.setState({ gasEstimate, page: 2 })
       })
@@ -47,8 +48,9 @@ export default class ModalParticipate extends Component {
   }
 
   submitForm(e, ...args) {
+    const { purchaseParticipationTokens } = this.props
     e.preventDefault()
-    this.props.purchaseParticipationTokens(this.state.quantity, false, (err, res) => {
+    purchaseParticipationTokens(this.state.quantity, false, (err, res) => {
       console.log('onSuccess for purchaseParticipationTokens', err, res)
     })
   }
@@ -59,7 +61,8 @@ export default class ModalParticipate extends Component {
   }
 
   validateForm(quantity) {
-    const bnRep = WrappedBigNumber(this.props.rep, 10)
+    const { rep } = this.props
+    const bnRep = WrappedBigNumber(rep, 10)
     const errors = []
     let isValid = true
 
@@ -91,7 +94,8 @@ export default class ModalParticipate extends Component {
   }
 
   handleMaxClick() {
-    this.setState({ quantity: this.props.rep, isValid: true, errors: [] })
+    const { rep } = this.props
+    this.setState({ quantity: rep, isValid: true, errors: [] })
   }
 
   handleKeyDown(e) {
@@ -103,7 +107,7 @@ export default class ModalParticipate extends Component {
   }
 
   render() {
-    const p = this.props
+    const { closeModal } = this.props
     const s = this.state
     const invalidWithErrors = (!s.isValid && s.errors.length > 0)
 
@@ -145,7 +149,7 @@ export default class ModalParticipate extends Component {
             <div className={Styles['ModalParticipate__form-actions']}>
               <button
                 className={Styles.ModalParticipate__button}
-                onClick={() => p.closeModal()}
+                onClick={() => closeModal()}
               >
                 Cancel
               </button>

src/modules/trade/components/trading--form/trading--form.jsx
@@ -13,20 +13,21 @@ import Styles from 'modules/trade/components/trading--form/trading--form.styles'
 
 class MarketTradingForm extends Component {
   static propTypes = {
+    availableFunds: PropTypes.instanceOf(BigNumber).isRequired,
+    isMobile: PropTypes.bool.isRequired,
     market: PropTypes.object.isRequired,
+    marketQuantity: PropTypes.instanceOf(BigNumber).isRequired,
     marketType: PropTypes.string.isRequired,
-    selectedNav: PropTypes.string.isRequired,
-    orderType: PropTypes.string.isRequired,
+    maxPrice: PropTypes.number.isRequired,
+    minPrice: PropTypes.number.isRequired,
+    nextPage: PropTypes.func.isRequired,
+    orderEstimate: PropTypes.string.isRequired,
     orderPrice: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]).isRequired,
     orderQuantity: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]).isRequired,
-    orderEstimate: PropTypes.string.isRequired,
+    orderType: PropTypes.string.isRequired,
+    selectedNav: PropTypes.string.isRequired,
     selectedOutcome: PropTypes.object.isRequired,
-    nextPage: PropTypes.func.isRequired,
     updateState: PropTypes.func.isRequired,
-    isMobile: PropTypes.bool.isRequired,
-    minPrice: PropTypes.number.isRequired,
-    maxPrice: PropTypes.number.isRequired,
-    availableFunds: PropTypes.instanceOf(BigNumber).isRequired,
   }
 
   constructor(props) {
@@ -56,6 +57,12 @@ class MarketTradingForm extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    const {
+      orderEstimate,
+      selectedNav,
+      selectedOutcome,
+      updateState,
+    } = this.props
     const newStateInfo = {
       [this.INPUT_TYPES.QUANTITY]: nextProps[this.INPUT_TYPES.QUANTITY],
       [this.INPUT_TYPES.PRICE]: nextProps[this.INPUT_TYPES.PRICE],
@@ -72,8 +79,8 @@ class MarketTradingForm extends Component {
       ...newStateInfo,
     }
     const currentOrderInfo = {
-      orderEstimate: this.props.orderEstimate,
-      selectedNav: this.props.selectedNav,
+      orderEstimate,
+      selectedNav,
       ...currentStateInfo,
     }
 
@@ -82,7 +89,7 @@ class MarketTradingForm extends Component {
       this.updateTrade(newStateInfo, nextProps)
 
       const nextTradePrice = nextProps.selectedOutcome.trade.limitPrice
-      const prevTradePrice = this.props.selectedOutcome.trade.limitPrice
+      const prevTradePrice = selectedOutcome.trade.limitPrice
       // limitPrice is being defaulted and we had no value in the input box
       const priceChange = (prevTradePrice === null && nextTradePrice !== null)
       // limitPrice is being updated in the background, but we have no limitPrice input set.
@@ -90,7 +97,7 @@ class MarketTradingForm extends Component {
 
       if ((priceChange || forcePriceUpdate)) {
         // if limitPrice input hasn't been changed and we have defaulted the limitPrice, populate the field so as to not confuse the user as to where estimates are coming from.
-        this.props.updateState(this.INPUT_TYPES.PRICE, WrappedBigNumber(nextTradePrice))
+        updateState(this.INPUT_TYPES.PRICE, WrappedBigNumber(nextTradePrice))
       }
 
       // orderValidation
@@ -113,13 +120,17 @@ class MarketTradingForm extends Component {
   }
 
   testPrice(value, errors, isOrderValid) {
+    const {
+      maxPrice,
+      minPrice,
+    } = this.props
     let errorCount = 0
     let passedTest = !!isOrderValid
     if (isNaN(value)) return { isOrderValid: false, errors, errorCount }
-    if (value && (value.lt(this.props.minPrice) || value.gt(this.props.maxPrice))) {
+    if (value && (value.lt(minPrice) || value.gt(maxPrice))) {
       errorCount += 1
       passedTest = false
-      errors[this.INPUT_TYPES.PRICE].push(`Price must be between ${this.props.minPrice} - ${this.props.maxPrice}`)
+      errors[this.INPUT_TYPES.PRICE].push(`Price must be between ${minPrice} - ${maxPrice}`)
     }
     return { isOrderValid: passedTest, errors, errorCount }
   }
@@ -161,6 +172,7 @@ class MarketTradingForm extends Component {
   }
 
   validateForm(property, rawValue) {
+    const { updateState } = this.props
     let value = rawValue
     if (!(BigNumber.isBigNumber(value)) && value !== '') value = WrappedBigNumber(value)
     const updatedState = {
@@ -170,7 +182,7 @@ class MarketTradingForm extends Component {
     const { isOrderValid, errors, errorCount } = this.orderValidation(updatedState, this.props)
     // update the state of the parent component to reflect new property/value
     // only update the trade if there were no errors detected.
-    this.props.updateState(property, value)
+    updateState(property, value)
 
     if (errorCount === 0) {
       this.updateTrade(updatedState)
@@ -186,21 +198,30 @@ class MarketTradingForm extends Component {
     })
   }
   render() {
-    const p = this.props
+    const {
+      isMobile,
+      market,
+      marketQuantity,
+      marketType,
+      nextPage,
+      orderEstimate,
+      orderType,
+      selectedOutcome,
+    } = this.props
     const s = this.state
 
-    const tickSize = parseFloat(p.market.tickSize)
+    const tickSize = parseFloat(market.tickSize)
     const errors = Array.from(new Set([...s.errors[this.INPUT_TYPES.QUANTITY], ...s.errors[this.INPUT_TYPES.PRICE], ...s.errors[this.INPUT_TYPES.MARKET_ORDER_SIZE]]))
 
     return (
       <ul className={Styles['TradingForm__form-body']}>
-        { !p.isMobile && p.market.marketType !== SCALAR &&
+        { !isMobile && market.marketType !== SCALAR &&
           <li>
             <label>Outcome</label>
-            <div className={Styles['TradingForm__static-field']}>{ p.selectedOutcome.name }</div>
+            <div className={Styles['TradingForm__static-field']}>{ selectedOutcome.name }</div>
           </li>
         }
-        { p.orderType === MARKET &&
+        { orderType === MARKET &&
           <li>
             <label htmlFor="tr__input--total-cost">Total Cost</label>
             <input
@@ -208,13 +229,13 @@ class MarketTradingForm extends Component {
               id="tr__input--total-cost"
               type="number"
               step={tickSize}
-              placeholder={`${p.marketType === SCALAR ? tickSize : '0.0001'} ETH`}
+              placeholder={`${marketType === SCALAR ? tickSize : '0.0001'} ETH`}
               value={BigNumber.isBigNumber(s[this.INPUT_TYPES.MARKET_ORDER_SIZE]) ? s[this.INPUT_TYPES.MARKET_ORDER_SIZE].toNumber() : s[this.INPUT_TYPES.MARKET_ORDER_SIZE]}
               onChange={e => this.validateForm(this.INPUT_TYPES.MARKET_ORDER_SIZE, e.target.value)}
             />
           </li>
         }
-        { p.orderType === LIMIT &&
+        { orderType === LIMIT &&
           <li>
             <label htmlFor="tr__input--quantity">Quantity</label>
             <input
@@ -222,13 +243,13 @@ class MarketTradingForm extends Component {
               id="tr__input--quantity"
               type="number"
               step={tickSize}
-              placeholder={`${p.marketType === SCALAR ? tickSize : '0.0001'} Shares`}
+              placeholder={`${marketType === SCALAR ? tickSize : '0.0001'} Shares`}
               value={BigNumber.isBigNumber(s[this.INPUT_TYPES.QUANTITY]) ? s[this.INPUT_TYPES.QUANTITY].toNumber() : s[this.INPUT_TYPES.QUANTITY]}
               onChange={e => this.validateForm(this.INPUT_TYPES.QUANTITY, e.target.value)}
             />
           </li>
         }
-        { p.orderType === LIMIT &&
+        { orderType === LIMIT &&
           <li>
             <label htmlFor="tr__input--limit-price">Limit Price</label>
             <input
@@ -236,22 +257,22 @@ class MarketTradingForm extends Component {
               id="tr__input--limit-price"
               type="number"
               step={tickSize}
-              placeholder={`${p.marketType === SCALAR ? tickSize : '0.0001'} ETH`}
+              placeholder={`${marketType === SCALAR ? tickSize : '0.0001'} ETH`}
               value={BigNumber.isBigNumber(s[this.INPUT_TYPES.PRICE]) ? s[this.INPUT_TYPES.PRICE].toNumber() : s[this.INPUT_TYPES.PRICE]}
               onChange={e => this.validateForm(this.INPUT_TYPES.PRICE, e.target.value)}
             />
           </li>
         }
-        { p.orderType === LIMIT &&
+        { orderType === LIMIT &&
           <li>
             <label>Est. Cost</label>
-            <div className={Styles['TradingForm__static-field']}>{ p.orderEstimate }</div>
+            <div className={Styles['TradingForm__static-field']}>{ orderEstimate }</div>
           </li>
         }
-        { p.orderType === MARKET &&
+        { orderType === MARKET &&
           <li>
             <label>Quantity</label>
-            <div className={Styles['TradingForm__static-field']}>{ p.marketQuantity }</div>
+            <div className={Styles['TradingForm__static-field']}>{ marketQuantity }</div>
           </li>
         }
         { errors.length > 0 &&
@@ -262,7 +283,7 @@ class MarketTradingForm extends Component {
         <li className={Styles['TradingForm__button--review']}>
           <button
             disabled={(!s.isOrderValid)}
-            onClick={s.isOrderValid ? p.nextPage : undefined}
+            onClick={s.isOrderValid ? nextPage : undefined}
           >Review
           </button>
         </li>

AugurProject/augur-ui

src/modules/categories/components/category/category.jsx
@@ -15,6 +15,7 @@ import { CATEGORY_PARAM_NAME } from 'modules/filter-sort/constants/param-names'
 
 export default class Category extends Component {
   static propTypes = {
+    category: PropTypes.string,
     popularity: PropTypes.string,
   }
 
@@ -29,7 +30,8 @@ export default class Category extends Component {
   }
 
   componentWillUpdate(nextProps, nextState) {
-    if (this.props.popularity !== nextProps.popularity) this.updatePopularity(nextProps.popularity > this.props.popularity ? CATEGORY_VOLUME_INCREASED : CATEGORY_VOLUME_DECREASED)
+    const { popularity } = this.props
+    if (popularity !== nextProps.popularity) this.updatePopularity(nextProps.popularity > popularity ? CATEGORY_VOLUME_INCREASED : CATEGORY_VOLUME_DECREASED)
   }
 
   updatePopularity(popularityChange) {
@@ -37,11 +39,14 @@ export default class Category extends Component {
   }
 
   render() {
-    const p = this.props
+    const {
+      category,
+      popularity,
+    } = this.props
     const s = this.state
 
-    const isNullCategory = p.category === 'null-category' && p.popularity === 0
-    const roundedPop = createBigNumber(p.popularity.toString()).integerValue(BigNumber.ROUND_HALF_EVEN)
+    const isNullCategory = category === 'null-category' && popularity === 0
+    const roundedPop = createBigNumber(popularity.toString()).integerValue(BigNumber.ROUND_HALF_EVEN)
     let popString = roundedPop.toNumber() === 1 ? ' SHARE' : ' SHARES'
     if (roundedPop > 1000) {
       const thousands = roundedPop / 1000
@@ -56,7 +61,7 @@ export default class Category extends Component {
         to={{
           pathname: makePath(MARKETS),
           search: makeQuery({
-            [CATEGORY_PARAM_NAME]: p.category,
+            [CATEGORY_PARAM_NAME]: category,
           }),
         }}
         className={isNullCategory ? Styles['Category__link-hidden'] : Styles.Category__link}
@@ -66,7 +71,7 @@ export default class Category extends Component {
         >
           <div className={Styles.Category__name} >
             <span ref={(categoryName) => { this.categoryName = categoryName }}>
-              {p.category.toUpperCase()}
+              {category.toUpperCase()}
             </span>
           </div>
           <div className={Styles.Category__separator} />

src/modules/modal/components/modal-participate/modal-participate.jsx
@@ -37,9 +37,10 @@ export default class ModalParticipate extends Component {
   }
 
   triggerReview(e, ...args) {
+    const { purchaseParticipationTokens } = this.props
     e.preventDefault()
     if (this.state.isValid) {
-      this.props.purchaseParticipationTokens(this.state.quantity, true, (err, gasEstimate) => {
+      purchaseParticipationTokens(this.state.quantity, true, (err, gasEstimate) => {
         console.log('trigger review', err, gasEstimate)
         if (!err && !!gasEstimate) this.setState({ gasEstimate, page: 2 })
       })
@@ -47,8 +48,9 @@ export default class ModalParticipate extends Component {
   }
 
   submitForm(e, ...args) {
+    const { purchaseParticipationTokens } = this.props
     e.preventDefault()
-    this.props.purchaseParticipationTokens(this.state.quantity, false, (err, res) => {
+    purchaseParticipationTokens(this.state.quantity, false, (err, res) => {
       console.log('onSuccess for purchaseParticipationTokens', err, res)
     })
   }
@@ -59,7 +61,8 @@ export default class ModalParticipate extends Component {
   }
 
   validateForm(quantity) {
-    const bnRep = createBigNumber(this.props.rep, 10)
+    const { rep } = this.props
+    const bnRep = createBigNumber(rep, 10)
     const errors = []
     let isValid = true
 
@@ -91,7 +94,8 @@ export default class ModalParticipate extends Component {
   }
 
   handleMaxClick() {
-    this.setState({ quantity: this.props.rep, isValid: true, errors: [] })
+    const { rep } = this.props
+    this.setState({ quantity: rep, isValid: true, errors: [] })
   }
 
   handleKeyDown(e) {
@@ -103,7 +107,7 @@ export default class ModalParticipate extends Component {
   }
 
   render() {
-    const p = this.props
+    const { closeModal } = this.props
     const s = this.state
     const invalidWithErrors = (!s.isValid && s.errors.length > 0)
 
@@ -145,7 +149,7 @@ export default class ModalParticipate extends Component {
             <div className={Styles['ModalParticipate__form-actions']}>
               <button
                 className={Styles.ModalParticipate__button}
-                onClick={() => p.closeModal()}
+                onClick={() => closeModal()}
               >
                 Cancel
               </button>

src/modules/trade/components/trading--form/trading--form.jsx
@@ -13,20 +13,21 @@ import Styles from 'modules/trade/components/trading--form/trading--form.styles'
 
 class MarketTradingForm extends Component {
   static propTypes = {
+    availableFunds: PropTypes.instanceOf(BigNumber).isRequired,
+    isMobile: PropTypes.bool.isRequired,
     market: PropTypes.object.isRequired,
+    marketQuantity: PropTypes.instanceOf(BigNumber).isRequired,
     marketType: PropTypes.string.isRequired,
-    selectedNav: PropTypes.string.isRequired,
-    orderType: PropTypes.string.isRequired,
+    maxPrice: PropTypes.number.isRequired,
+    minPrice: PropTypes.number.isRequired,
+    nextPage: PropTypes.func.isRequired,
+    orderEstimate: PropTypes.string.isRequired,
     orderPrice: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]).isRequired,
     orderQuantity: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]).isRequired,
-    orderEstimate: PropTypes.string.isRequired,
+    orderType: PropTypes.string.isRequired,
+    selectedNav: PropTypes.string.isRequired,
     selectedOutcome: PropTypes.object.isRequired,
-    nextPage: PropTypes.func.isRequired,
     updateState: PropTypes.func.isRequired,
-    isMobile: PropTypes.bool.isRequired,
-    minPrice: PropTypes.number.isRequired,
-    maxPrice: PropTypes.number.isRequired,
-    availableFunds: PropTypes.instanceOf(BigNumber).isRequired,
   }
 
   constructor(props) {
@@ -56,6 +57,12 @@ class MarketTradingForm extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    const {
+      orderEstimate,
+      selectedNav,
+      selectedOutcome,
+      updateState,
+    } = this.props
     const newStateInfo = {
       [this.INPUT_TYPES.QUANTITY]: nextProps[this.INPUT_TYPES.QUANTITY],
       [this.INPUT_TYPES.PRICE]: nextProps[this.INPUT_TYPES.PRICE],
@@ -72,8 +79,8 @@ class MarketTradingForm extends Component {
       ...newStateInfo,
     }
     const currentOrderInfo = {
-      orderEstimate: this.props.orderEstimate,
-      selectedNav: this.props.selectedNav,
+      orderEstimate,
+      selectedNav,
       ...currentStateInfo,
     }
 
@@ -82,7 +89,7 @@ class MarketTradingForm extends Component {
       this.updateTrade(newStateInfo, nextProps)
 
       const nextTradePrice = nextProps.selectedOutcome.trade.limitPrice
-      const prevTradePrice = this.props.selectedOutcome.trade.limitPrice
+      const prevTradePrice = selectedOutcome.trade.limitPrice
       // limitPrice is being defaulted and we had no value in the input box
       const priceChange = (prevTradePrice === null && nextTradePrice !== null)
       // limitPrice is being updated in the background, but we have no limitPrice input set.
@@ -90,7 +97,7 @@ class MarketTradingForm extends Component {
 
       if ((priceChange || forcePriceUpdate)) {
         // if limitPrice input hasn't been changed and we have defaulted the limitPrice, populate the field so as to not confuse the user as to where estimates are coming from.
-        this.props.updateState(this.INPUT_TYPES.PRICE, createBigNumber(nextTradePrice))
+        updateState(this.INPUT_TYPES.PRICE, createBigNumber(nextTradePrice))
       }
 
       // orderValidation
@@ -113,13 +120,17 @@ class MarketTradingForm extends Component {
   }
 
   testPrice(value, errors, isOrderValid) {
+    const {
+      maxPrice,
+      minPrice,
+    } = this.props
     let errorCount = 0
     let passedTest = !!isOrderValid
     if (isNaN(value)) return { isOrderValid: false, errors, errorCount }
-    if (value && (value.lt(this.props.minPrice) || value.gt(this.props.maxPrice))) {
+    if (value && (value.lt(minPrice) || value.gt(maxPrice))) {
       errorCount += 1
       passedTest = false
-      errors[this.INPUT_TYPES.PRICE].push(`Price must be between ${this.props.minPrice} - ${this.props.maxPrice}`)
+      errors[this.INPUT_TYPES.PRICE].push(`Price must be between ${minPrice} - ${maxPrice}`)
     }
     return { isOrderValid: passedTest, errors, errorCount }
   }
@@ -161,6 +172,7 @@ class MarketTradingForm extends Component {
   }
 
   validateForm(property, rawValue) {
+    const { updateState } = this.props
     let value = rawValue
     if (!(BigNumber.isBigNumber(value)) && value !== '') value = createBigNumber(value)
     const updatedState = {
@@ -170,7 +182,7 @@ class MarketTradingForm extends Component {
     const { isOrderValid, errors, errorCount } = this.orderValidation(updatedState, this.props)
     // update the state of the parent component to reflect new property/value
     // only update the trade if there were no errors detected.
-    this.props.updateState(property, value)
+    updateState(property, value)
 
     if (errorCount === 0) {
       this.updateTrade(updatedState)
@@ -186,21 +198,30 @@ class MarketTradingForm extends Component {
     })
   }
   render() {
-    const p = this.props
+    const {
+      isMobile,
+      market,
+      marketQuantity,
+      marketType,
+      nextPage,
+      orderEstimate,
+      orderType,
+      selectedOutcome,
+    } = this.props
     const s = this.state
 
-    const tickSize = parseFloat(p.market.tickSize)
+    const tickSize = parseFloat(market.tickSize)
     const errors = Array.from(new Set([...s.errors[this.INPUT_TYPES.QUANTITY], ...s.errors[this.INPUT_TYPES.PRICE], ...s.errors[this.INPUT_TYPES.MARKET_ORDER_SIZE]]))
 
     return (
       <ul className={Styles['TradingForm__form-body']}>
-        { !p.isMobile && p.market.marketType !== SCALAR &&
+        { !isMobile && market.marketType !== SCALAR &&
           <li>
             <label>Outcome</label>
-            <div className={Styles['TradingForm__static-field']}>{ p.selectedOutcome.name }</div>
+            <div className={Styles['TradingForm__static-field']}>{ selectedOutcome.name }</div>
           </li>
         }
-        { p.orderType === MARKET &&
+        { orderType === MARKET &&
           <li>
             <label htmlFor="tr__input--total-cost">Total Cost</label>
             <input
@@ -208,13 +229,13 @@ class MarketTradingForm extends Component {
               id="tr__input--total-cost"
               type="number"
               step={tickSize}
-              placeholder={`${p.marketType === SCALAR ? tickSize : '0.0001'} ETH`}
+              placeholder={`${marketType === SCALAR ? tickSize : '0.0001'} ETH`}
               value={BigNumber.isBigNumber(s[this.INPUT_TYPES.MARKET_ORDER_SIZE]) ? s[this.INPUT_TYPES.MARKET_ORDER_SIZE].toNumber() : s[this.INPUT_TYPES.MARKET_ORDER_SIZE]}
               onChange={e => this.validateForm(this.INPUT_TYPES.MARKET_ORDER_SIZE, e.target.value)}
             />
           </li>
         }
-        { p.orderType === LIMIT &&
+        { orderType === LIMIT &&
           <li>
             <label htmlFor="tr__input--quantity">Quantity</label>
             <input
@@ -222,13 +243,13 @@ class MarketTradingForm extends Component {
               id="tr__input--quantity"
               type="number"
               step={tickSize}
-              placeholder={`${p.marketType === SCALAR ? tickSize : '0.0001'} Shares`}
+              placeholder={`${marketType === SCALAR ? tickSize : '0.0001'} Shares`}
               value={BigNumber.isBigNumber(s[this.INPUT_TYPES.QUANTITY]) ? s[this.INPUT_TYPES.QUANTITY].toNumber() : s[this.INPUT_TYPES.QUANTITY]}
               onChange={e => this.validateForm(this.INPUT_TYPES.QUANTITY, e.target.value)}
             />
           </li>
         }
-        { p.orderType === LIMIT &&
+        { orderType === LIMIT &&
           <li>
             <label htmlFor="tr__input--limit-price">Limit Price</label>
             <input
@@ -236,22 +257,22 @@ class MarketTradingForm extends Component {
               id="tr__input--limit-price"
               type="number"
               step={tickSize}
-              placeholder={`${p.marketType === SCALAR ? tickSize : '0.0001'} ETH`}
+              placeholder={`${marketType === SCALAR ? tickSize : '0.0001'} ETH`}
               value={BigNumber.isBigNumber(s[this.INPUT_TYPES.PRICE]) ? s[this.INPUT_TYPES.PRICE].toNumber() : s[this.INPUT_TYPES.PRICE]}
               onChange={e => this.validateForm(this.INPUT_TYPES.PRICE, e.target.value)}
             />
           </li>
         }
-        { p.orderType === LIMIT &&
+        { orderType === LIMIT &&
           <li>
             <label>Est. Cost</label>
-            <div className={Styles['TradingForm__static-field']}>{ p.orderEstimate }</div>
+            <div className={Styles['TradingForm__static-field']}>{ orderEstimate }</div>
           </li>
         }
-        { p.orderType === MARKET &&
+        { orderType === MARKET &&
           <li>
             <label>Quantity</label>
-            <div className={Styles['TradingForm__static-field']}>{ p.marketQuantity }</div>
+            <div className={Styles['TradingForm__static-field']}>{ marketQuantity }</div>
           </li>
         }
         { errors.length > 0 &&
@@ -262,7 +283,7 @@ class MarketTradingForm extends Component {
         <li className={Styles['TradingForm__button--review']}>
           <button
             disabled={(!s.isOrderValid)}
-            onClick={s.isOrderValid ? p.nextPage : undefined}
+            onClick={s.isOrderValid ? nextPage : undefined}
           >Review
           </button>
         </li>