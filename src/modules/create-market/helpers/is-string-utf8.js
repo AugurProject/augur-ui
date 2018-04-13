f35ed96956899b47ac2d74a3f741d08c42d76348
Tom Haile <bradford.haile@gmail.com>
2018-04-13 15:52:39 -0500

    fixed very edge case where description is only emojis, remove debug info in test

--- src/modules/create-market/helpers/is-string-utf8.js
+++ src/modules/create-market/helpers/is-string-utf8.js
@@ -1,6 +1,5 @@
 export const isStringUtf8 = (stringValue) => {
   if (typeof stringValue === 'number') return true
   const buf = Buffer.from(stringValue)
-  console.log('buf', buf.toString(), 'buf')
   return Buffer.compare(Buffer.from(buf.toString(), 'utf8'), buf) === 0
 }
