a7f1648ad2df6ebb49674e965f4887cec67a54d6
adrake33 <aarondrake@gmail.com>
2018-05-15 17:59:59 -0700

    Reverting some changes to resolve the original bug

--- src/modules/portfolio/components/portfolio-reports/portfolio-reports.jsx
+++ src/modules/portfolio/components/portfolio-reports/portfolio-reports.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
-import BigNumber from 'bignumber.js'
 
 import { formatAttoRep, formatEther } from 'utils/format-number'
 
