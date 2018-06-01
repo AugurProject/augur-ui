fb225fad9b70c2977e712956c152b6d6df11dad9
John Danz <John.Danz87@gmail.com>
2018-06-01 10:04:37 -0500

    fixed issue with trading form

--- src/modules/trade/components/trading--form/trading--form.jsx
+++ src/modules/trade/components/trading--form/trading--form.jsx
@@ -115,7 +115,7 @@ class MarketTradingForm extends Component {
   testQuantity(value, errors, isOrderValid) {
     let errorCount = 0
     let passedTest = !!isOrderValid
-    if (isNaN(value)) return { isOrderValid: false, errors, errorCount }
+    if (!BigNumber.isBigNumber(value)) return { isOrderValid: false, errors, errorCount }
     if (value && value.lte(0)) {
       errorCount += 1
       passedTest = false
@@ -133,7 +133,7 @@ class MarketTradingForm extends Component {
     const tickSize = createBigNumber(market.tickSize)
     let errorCount = 0
     let passedTest = !!isOrderValid
-    if (isNaN(value)) return { isOrderValid: false, errors, errorCount }
+    if (!BigNumber.isBigNumber(value)) return { isOrderValid: false, errors, errorCount }
     if (value && (value.lte(minPrice) || value.gte(maxPrice))) {
       errorCount += 1
       passedTest = false
@@ -198,7 +198,7 @@ class MarketTradingForm extends Component {
     // update the state of the parent component to reflect new property/value
     // only update the trade if there were no errors detected.
     updateState(property, value)
-
+    console.log(errorCount, updatedState);
     if (errorCount === 0) {
       this.updateTrade(updatedState)
     }
