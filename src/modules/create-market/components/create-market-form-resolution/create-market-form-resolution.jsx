ceacdcaeaf050fd0a8a57e820b7921dec6dbc34b
Tom Haile <bradford.haile@gmail.com>
2018-08-13 12:28:29 -0500

    Update create-market-form-resolution.jsx
    moving back to 6 months.

--- src/modules/create-market/components/create-market-form-resolution/create-market-form-resolution.jsx
+++ src/modules/create-market/components/create-market-form-resolution/create-market-form-resolution.jsx
@@ -251,7 +251,7 @@ export default class CreateMarketResolution extends Component {
               >
                 <h4>Market Expiration Date</h4>
                 <p style={{ color: '#372e4b' }}>
-                  {'Augur is still experimental software, creating markets past one month has been disabled to discourage long running markets.'}
+                  {'Augur is still experimental software, creating markets past six months has been disabled to discourage long running markets.'}
                 </p>
               </ReactTooltip>
             </div>
@@ -267,7 +267,7 @@ export default class CreateMarketResolution extends Component {
               validateField('endTime', formatDate(date.toDate()))
             }}
             isOutsideRange={day =>
-              day.isAfter(moment(this.props.currentTimestamp).add(1, 'M')) ||
+              day.isAfter(moment(this.props.currentTimestamp).add(6, 'M')) ||
               day.isBefore(moment(this.props.currentTimestamp))
             }
             focused={this.state.focused}
