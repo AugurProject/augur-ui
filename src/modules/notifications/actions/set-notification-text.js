dc9ffe971ba40a41574567ffb9d9f486c58537d6
adrake33 <aarondrake@gmail.com>
2018-08-27 08:57:45 -0700

    Add faucet notification type

--- src/modules/notifications/actions/set-notification-text.js
+++ src/modules/notifications/actions/set-notification-text.js
@@ -188,6 +188,11 @@ export default function setNotificationText(notification, callback) {
         notification.title = "Migrate REP";
         break;
 
+      // LegacyReputationToken
+      case "FAUCET":
+        notification.title = "Get REP from faucet";
+        break;
+
       // Mailbox
       case "WITHDRAWETHER":
         notification.title = "Withdraw ETH from mailbox";
