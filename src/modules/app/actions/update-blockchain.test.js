95ad98419ac8adea755395db4463c9880b9c2193
unparallel <juliya@augur.net>
2018-09-12 18:49:03 -0500

    Migrated update-blockchain-test to jest

--- src/modules/app/actions/update-blockchain.test.js
+++ src/modules/app/actions/update-blockchain.test.js
@@ -1,26 +1,22 @@
-import proxyquire from "proxyquire";
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
 import testState from "test/testState";
+import { updateBlockchain } from "./update-blockchain";
 
 describe(`modules/app/actions/update-blockchain.js`, () => {
-  proxyquire.noPreserveCache().noCallThru();
   const middlewares = [thunk];
   const mockStore = configureMockStore(middlewares);
   const state = Object.assign({}, testState);
   const store = mockStore(state);
-  const action = proxyquire(
-    "../../../src/modules/app/actions/update-blockchain.js",
-    {}
-  );
-  it("should dispatch UPDATE_BLOCKCHAIN action", () => {
+  const action = updateBlockchain;
+  test("should dispatch UPDATE_BLOCKCHAIN action", () => {
     store.dispatch(
-      action.updateBlockchain({
+      action({
         currentBlockNumber: 10000,
         currentBlockTimestamp: 4886718345
       })
     );
-    assert.deepEqual(store.getActions(), [
+    expect(store.getActions()).toEqual([
       {
         type: "UPDATE_BLOCKCHAIN",
         data: {
