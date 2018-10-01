a746200f28158ca6b2dffbc2817c1108b97a8270
unparallel <juliya@augur.net>
2018-10-01 17:51:08 -0500

    Jest config coverage

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -21,7 +21,7 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: null,
+  collectCoverageFrom: ["<rootDir>/src/**/*.test.js?(x)"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
