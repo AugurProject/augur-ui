64beec90a753dcc7d19050db07128e0b7c7047e0
Tom Haile <bradford.haile@gmail.com>
2018-10-02 16:24:19 -0500

    fixed data issue showing last price twice

--- src/modules/market/components/market-positions-list--position/market-positions-list--position.jsx
+++ src/modules/market/components/market-positions-list--position/market-positions-list--position.jsx
@@ -65,17 +65,16 @@ export default class MarketPositionsListPosition extends Component {
         <li>{netPositionShares}</li>
         <li>{positionShares}</li>
         <li>{getValue(position, "purchasePrice.formatted")}</li>
-        {isExtendedDisplay &&
-          !isMobile && <li>{getValue(outcome, "lastPrice.formatted")}</li>}
-        {!isMobile && (
-          <li>
-            {getValue(outcome, "lastPrice.formatted")}
-            <MarketOutcomeTradingIndicator
-              outcome={outcome}
-              location="positions"
-            />
-          </li>
-        )}
+        {!isMobile &&
+          isExtendedDisplay && (
+            <li>
+              {getValue(outcome, "lastPrice.formatted")}
+              <MarketOutcomeTradingIndicator
+                outcome={outcome}
+                location="positions"
+              />
+            </li>
+          )}
         {!isMobile && <li>{getValue(position, "unrealizedNet.formatted")} </li>}
         {!isMobile && <li>{getValue(position, "realizedNet.formatted")} </li>}
         {isExtendedDisplay && (
