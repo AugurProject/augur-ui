97c61c85da0ffe65850f56ad66747d317ebb1802
Tom Haile <bradford.haile@gmail.com>
2018-10-09 12:57:09 -0500

    removed unneeded styling

--- src/modules/modal/components/common/modal-receipt.jsx
+++ src/modules/modal/components/common/modal-receipt.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import Styles from "modules/modal/components/common/common.styles";
 
 const ModalReceipt = p => (
-  <div className={Styles.ReceiptContainer}>
+  <div>
     <ul className={Styles.Receipt}>
       {p.items.map(item => (
         <li key={item.label}>
