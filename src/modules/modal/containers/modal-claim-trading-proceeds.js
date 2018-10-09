ea7958b63c2a0821f889e3e492b339a1b3722f0c
Tom Haile <bradford.haile@gmail.com>
2018-10-09 14:22:43 -0500

    use same value displayed in position footer for user winnings, remove selector and test

--- src/modules/modal/containers/modal-claim-trading-proceeds.js
+++ src/modules/modal/containers/modal-claim-trading-proceeds.js
@@ -1,30 +1,23 @@
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import ModalReview from "modules/modal/components/modal-review";
+import { selectMarket } from "modules/markets/selectors/market";
 import claimTradingProceeds, {
   CLAIM_SHARES_GAS_COST
 } from "modules/positions/actions/claim-trading-proceeds";
 import { closeModal } from "modules/modal/actions/close-modal";
-import selectWinningPositions from "modules/positions/selectors/winning-positions";
 import { getGasPrice } from "modules/auth/selectors/get-gas-price";
 import { formatGasCostToEther } from "utils/format-number";
 import { MODAL_REVIEW } from "modules/modal/constants/modal-types";
 
-const mapStateToProps = state => {
-  const winningPositions = selectWinningPositions();
-
-  const gasCost = formatGasCostToEther(
+const mapStateToProps = state => ({
+  modal: state.modal,
+  gasCost: formatGasCostToEther(
     CLAIM_SHARES_GAS_COST,
     { decimalsRounded: 4 },
     getGasPrice(state)
-  );
-
-  return {
-    modal: state.modal,
-    winningPositions,
-    gasCost
-  };
-};
+  )
+});
 
 const mapDispatchToProps = dispatch => ({
   closeModal: () => dispatch(closeModal()),
@@ -33,10 +26,12 @@ const mapDispatchToProps = dispatch => ({
 });
 
 const mergeProps = (sP, dP, oP) => {
-  const { winningPositions } = sP;
   const { marketId } = sP.modal;
+  const market = selectMarket(marketId);
+  const { description, outstandingReturns } = market;
+  const { gasCost } = sP;
   const { closeModal, claimTradingProceeds } = dP;
-  const winningPosition = winningPositions[marketId];
+
   return {
     buttons: [
       {
@@ -48,24 +43,24 @@ const mergeProps = (sP, dP, oP) => {
         label: "confirm",
         action: () => {
           closeModal();
-          claimTradingProceeds(winningPosition.id);
+          claimTradingProceeds(marketId);
         }
       }
     ],
     items: [
       {
         label: "Market",
-        value: winningPosition.description,
+        value: description,
         denomination: ""
       },
       {
         label: "Returns",
-        value: winningPosition.winnings.toString(),
+        value: outstandingReturns,
         denomination: "ETH"
       },
       {
         label: "Gas Cost",
-        value: sP.gasCost,
+        value: gasCost,
         denomination: "ETH"
       }
     ],
