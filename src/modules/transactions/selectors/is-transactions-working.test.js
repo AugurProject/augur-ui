8f3b02fa32e99b49819745e8c14f85c7fe83e336
unparallel <juliya@augur.net>
2018-10-11 19:43:57 -0500

    test.each isTransactionsWorking

--- src/modules/transactions/selectors/is-transactions-working.test.js
+++ src/modules/transactions/selectors/is-transactions-working.test.js
@@ -8,48 +8,47 @@ import * as mockStore from "test/mockStore";
 import isTransactionsWorkingAssertions from "assertions/is-transactions-working";
 
 describe(`modules/transactions/selectors/is-transaction-working.js`, () => {
-  let actual;
-
   const { state } = mockStore.default;
 
-  test("checked if a transaction is working", () => {
-    let { transactionsData } = state;
-    actual = selectIsWorking({ transactionsData });
-    expect(selectIsWorking({ transactionsData })).toBe(false);
-
-    transactionsData = {
+  const transactionDataArray = [
+    state.transactionsData,
+    {
       testtransaction12345: {
         id: "testtransaction12345",
         status: SUCCESS
       }
-    };
-    actual = selectIsWorking({ transactionsData });
-    expect(actual).toBe(false);
-
-    transactionsData = {
+    },
+    {
       testtransaction12345: {
         id: "testtransaction12345",
         status: PENDING
       }
-    };
-    actual = selectIsWorking({ transactionsData });
-    expect(actual).toBe(false);
-
-    transactionsData = {
+    },
+    {
       testtransaction12345: {
         id: "testtransaction12345",
         status: INTERRUPTED
       }
-    };
-    actual = selectIsWorking({ transactionsData });
-    expect(actual).toBe(false);
+    }
+  ];
 
-    transactionsData = {
-      testtransaction12345: {
-        id: "testtransaction12345",
-        status: "test"
-      }
-    };
+  let actual;
+
+  test.each([
+    [transactionDataArray[0], false],
+    [transactionDataArray[1], false],
+    [transactionDataArray[2], false],
+    [transactionDataArray[3], false]
+  ])("%o is %b", (tx, expected) => {
+    actual = selectIsWorking({ tx });
+    expect(selectIsWorking(actual)).toBe(expected);
+  });
+
+  test("assertions/is-transactions-working", () => {
+    const transactionsData = Object.assign(state.transactionsData, {
+      id: "testtransaction12345",
+      status: "test"
+    });
     actual = selectIsWorking({ transactionsData });
     isTransactionsWorkingAssertions(actual);
     expect(actual).toBe(true);
