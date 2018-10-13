d9685f90dde33d96696722666a8fcb39ebbcc6e6
Tom Haile <bradford.haile@gmail.com>
2018-10-13 13:23:43 -0500

    need toString() to convert number of more than 14 decimal points so big number doesn't explode

--- src/modules/market-charts/components/market-outcome-charts--candlestick/market-outcome-charts--candlestick.jsx
+++ src/modules/market-charts/components/market-outcome-charts--candlestick/market-outcome-charts--candlestick.jsx
@@ -588,8 +588,8 @@ function drawVolume({
     .attr("x", containerWidth - 110)
     .attr("y", d => yVolumeScale(d.volume) - 4)
     .text(d => {
-      if (createBigNumber(d.volume).gte(ONE)) {
-        return d.volume.toFixed(1) + ` ETH`;
+      if (createBigNumber(d.volume.toString()).gte(ONE)) {
+        return d.volume.toFixed(0) + ` ETH`;
       }
       return d.volume.toFixed(4) + ` ETH`;
     });
