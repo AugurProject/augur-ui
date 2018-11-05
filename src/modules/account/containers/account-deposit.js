6f41acfc98c51f8846f506f460be409681e91cc6
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-11-05 15:31:33 -0800

    Replace AirSwap and ShapeShift with 0x instant

--- src/modules/account/containers/account-deposit.js
+++ src/modules/account/containers/account-deposit.js
@@ -1,11 +1,36 @@
 import { connect } from "react-redux";
 
 import AccountDeposit from "modules/account/components/account-deposit/account-deposit";
+import { augur } from "services/augurjs";
+import { assetDataUtils } from "@0xproject/order-utils";
 
 const mapStateToProps = state => ({
   address: state.loginAccount.displayAddress
 });
 
-const AccountDepositContainer = connect(mapStateToProps)(AccountDeposit);
+const mapDispatchToProps = () => ({
+  openZeroExInstant: () => {
+    augur.api.Universe.getReputationToken((err, repTokenAddress) => {
+      if (err) return;
+
+      const assetData = assetDataUtils.encodeERC20AssetData(repTokenAddress);
+
+      // eslint-disable-next-line
+      zeroExInstant.render(
+        {
+          availableAssetDatas: [],
+          defaultSelectedAssetData: assetData,
+          orderSource: "https://api.radarrelay.com/0x/v2"
+        },
+        "body"
+      );
+    });
+  }
+});
+
+const AccountDepositContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AccountDeposit);
 
 export default AccountDepositContainer;
