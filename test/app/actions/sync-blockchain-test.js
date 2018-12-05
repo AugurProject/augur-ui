e9f541729c32c52343cc30d26ff6085b923e3fdf
phoebemirman <phoebe.mirman@gmail.com>
2018-12-05 10:57:14 -0800

    update test to jest

--- test/app/actions/sync-blockchain-test.js
+++ test/app/actions/sync-blockchain-test.js
@@ -1,38 +1,15 @@
-import {
-  syncBlockchain,
-  __RewireAPI__ as ReWireModule
-} from "modules/app/actions/sync-blockchain";
-import { describe, it, after } from "mocha";
+import { syncBlockchain } from "modules/app/actions/sync-blockchain";
 
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
 import testState from "test/testState";
 
-describe(`modules/app/actions/sync-blockchain.js`, () => {
-  // eslint-disable-line func-names, prefer-arrow-callback
-  const middlewares = [thunk];
-  const mockStore = configureMockStore(middlewares);
-  const state = Object.assign({}, testState, {
-    blockchain: {
-      currentBlockTimestamp: 4886718335,
-      currentBlockNumber: 9999,
-      currentAugurTimestamp: 42
-    },
-    gasPriceInfo: {
-      blockNumber: undefined
-    }
-  });
-  const dataReturned = {
-    currentBlockNumber: 0x10000,
-    currentBlockTimestamp: 0x4886718345,
-    currentAugurTimestamp: 42,
-  };
-  const syncData = {
-    highestBlock: 22,
-    lastProcessedBlock: 22,
-  }
-  const store = mockStore(state);
-  const AugurJS = {
+import * as updateBlockChainModule from "modules/app/actions/update-blockchain";
+import * as updateAssetsModule from "modules/auth/actions/update-assets";
+import * as loadGadPriceInfoModule from "modules/app/actions/load-gas-price-info";
+
+jest.mock("services/augurjs", () => ({
+  augur: {
     rpc: {
       getCurrentBlock: () => ({
         number: 10000,
@@ -40,7 +17,7 @@ describe(`modules/app/actions/sync-blockchain.js`, () => {
       }),
       block: {
         number: 10000,
-        timestamp: 4886718345
+        timestamp: "0x123456789"
       }
     },
     api: {
@@ -62,39 +39,72 @@ describe(`modules/app/actions/sync-blockchain.js`, () => {
         })
       }
     },
+  }
+}));
+
+jest.mock("modules/app/actions/update-blockchain");
+jest.mock("modules/auth/actions/update-assets");
+jest.mock("modules/app/actions/load-gas-price-info");
+
+describe(`modules/app/actions/sync-blockchain.js`, () => {
+  let updateBlockchainSpy;
+  let updateAssetsSpy;
+  let loadGasPriceInfoSpy;
+
+  const middlewares = [thunk];
+  const mockStore = configureMockStore(middlewares);
+  const state = Object.assign({}, testState, {
+    blockchain: {
+      currentBlockTimestamp: 4886718335,
+      currentBlockNumber: 9999,
+      currentAugurTimestamp: 42
+    },
+    gasPriceInfo: {
+      blockNumber: undefined
+    }
+  });
+  const dataReturned = {
+    currentBlockNumber: 0x10000,
+    currentBlockTimestamp: 0x4886718345,
+    currentAugurTimestamp: 42
   };
+  const syncData = {
+    highestBlock: 22,
+    lastProcessedBlock: 22,
+  };
+  const store = mockStore(state);
 
-  const updateBlockchain = data => ({
-    type: "UPDATE_BLOCKCHAIN",
-    data
-  });
-  const updateAssets = data => ({
-    type: "UPDATE_ASSETS"
-  });
-  const loadGasPriceInfo = () => ({
-    type: "UPDATE_GAS_PRICE_INFO"
-  });
-  ReWireModule.__Rewire__("augur", AugurJS);
-  ReWireModule.__Rewire__("updateBlockchain", updateBlockchain);
-  ReWireModule.__Rewire__("updateAssets", updateAssets);
-  ReWireModule.__Rewire__("loadGasPriceInfo", loadGasPriceInfo);
+  beforeAll(() => {
+    updateBlockchainSpy = jest
+      .spyOn(updateBlockChainModule, "updateBlockchain")
+      .mockImplementation(data => ({
+        type: "UPDATE_BLOCKCHAIN",
+        data
+      }));
 
-  after(() => {
+    updateAssetsSpy = jest
+      .spyOn(updateAssetsModule, "updateAssets")
+      .mockImplementation(() => ({
+        type: "UPDATE_ASSETS"
+      }));
+
+    loadGasPriceInfoSpy = jest
+      .spyOn(loadGadPriceInfoModule, "loadGasPriceInfo")
+      .mockImplementation(() => ({
+        type: "UPDATE_GAS_PRICE_INFO"
+      }));
+  });
+
+  afterAll(() => {
     store.clearActions();
-    ReWireModule.__ResetDependency__(
-      "augur",
-      "updateBlockchain",
-      "updateAssets",
-      "loadGasPriceInfo"
-    );
+    updateBlockchainSpy.mockReset();
+    updateAssetsSpy.mockReset();
+    loadGasPriceInfoSpy.mockReset();
   });
 
-  it("rpc.block set: should sync with blockchain using rpc.block.number", done => {
-    AugurJS.rpc.block = {
-      number: 10000,
-      timestamp: "0x123456789"
-    };
-    const out = [
+  test("rpc.block set: should sync with blockchain using rpc.block.number", done => {
+    store.dispatch(syncBlockchain());
+    expect(store.getActions()).toEqual([
       {
         type: "UPDATE_BLOCKCHAIN",
         data: dataReturned
@@ -109,14 +119,7 @@ describe(`modules/app/actions/sync-blockchain.js`, () => {
       {
         type: "UPDATE_ASSETS"
       }
-    ];
-    store.dispatch(syncBlockchain());
-    assert.deepEqual(
-      store.getActions(),
-      out,
-      `Didn't dispatch the expected actions`
-    );
-
+    ]);
     done();
   });
 });
