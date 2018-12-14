3925405c164788db8fb9a063c7d52433d101b500
phoebemirman <phoebe.mirman@gmail.com>
2018-12-14 14:16:14 -0800

    add brackets

--- src/modules/market/components/market-header/market-header.jsx
+++ src/modules/market/components/market-header/market-header.jsx
@@ -65,8 +65,9 @@ export default class MarketHeader extends Component {
   }
 
   updateDetailsHeight() {
-    if (this.detailsContainer)
+    if (this.detailsContainer) {
       this.setState({ detailsHeight: this.detailsContainer.scrollHeight });
+    }
   }
 
   toggleReadMore() {
