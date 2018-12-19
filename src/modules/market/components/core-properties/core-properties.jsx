718c04c878606a42d210e94ab9d15d6eb30b5ca4
Tom Haile <bradford.haile@gmail.com>
2018-12-18 21:43:37 -0600

    added scalar properties to market header

--- src/modules/market/components/core-properties/core-properties.jsx
+++ src/modules/market/components/core-properties/core-properties.jsx
@@ -2,176 +2,104 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-import classNames from "classnames";
 import { SCALAR, YES_NO } from "modules/markets/constants/market-types";
-
 import Styles from "modules/market/components/core-properties/core-properties.styles";
-import ReactTooltip from "react-tooltip";
-import TooltipStyles from "modules/common/less/tooltip.styles";
 import getValue from "utils/get-value";
-import { Hint } from "modules/common/components/icons";
 
-const Property = ({ numRow, property }) => (
-  <div
-    className={classNames(Styles.CoreProperties__property, {
-      [Styles.CoreProperties__propertySmall]: numRow !== 0
-    })}
-  >
-    <span className={Styles[`CoreProperties__property-name`]}>
-      <div>{property.name}</div>
-      {property.tooltip && (
+const CoreProperties = ({ market, isMobileSmall }) => (
+  <div className={Styles.CoreProperties__coreContainer}>
+    <div className={Styles.CoreProperties__row}>
+      <div className={Styles.CoreProperties__property}>
+        <span className={Styles[`CoreProperties__property-name`]}>
+          <div>Total Volume</div>
+        </span>
+        <span className={Styles[`CoreProperties__property-value-large`]}>
+          {getValue(market, "volume.formatted")}
+          <span className={Styles[`CoreProperties__property-denomination`]}>
+            ETH
+          </span>
+        </span>
+      </div>
+      <div className={Styles.CoreProperties__property}>
+        <span className={Styles[`CoreProperties__property-name`]}>
+          <div>Open Interest</div>
+        </span>
+        <span className={Styles[`CoreProperties__property-value-large`]}>
+          {getValue(market, "openInterest.formatted")}
+          <span className={Styles[`CoreProperties__property-denomination`]}>
+            ETH
+          </span>
+        </span>
+      </div>
+    </div>
+    <div className={Styles.CoreProperties__row}>
+      <div className={Styles.CoreProperties__property}>
+        <span className={Styles[`CoreProperties__property-name`]}>
+          <div>24hr Volume</div>
+        </span>
+        <span className={Styles[`CoreProperties__property-value`]}>
+          {getValue(market, "volume.formatted")}
+          <span className={Styles[`CoreProperties__property-denomination`]}>
+            ETH
+          </span>
+        </span>
+      </div>
+      <div className={Styles.CoreProperties__property}>
+        <span className={Styles[`CoreProperties__property-name`]}>
+          <div>Fee</div>
+        </span>
+        <span className={Styles[`CoreProperties__property-value`]}>
+          {getValue(market, "settlementFeePercent.full")}
+        </span>
+      </div>
+    </div>
+    <div className={Styles.CoreProperties__row}>
+      <div className={Styles.CoreProperties__property}>
+        <span className={Styles[`CoreProperties__property-name`]}>
+          <div>Type</div>
+        </span>
+        <span className={Styles[`CoreProperties__property-value`]}>
+          {getValue(market, "marketType") === YES_NO
+            ? "Yes/No"
+            : getValue(market, "marketType")}
+        </span>
+      </div>
+      {getValue(market, "marketType") === SCALAR && (
         <div>
-          <label
-            className={classNames(
-              TooltipStyles.TooltipHint,
-              Styles["CoreProperties__property-tooltip"]
-            )}
-            data-tip
-            data-for="tooltip--market-fees"
-          >
-            {Hint}
-          </label>
-          <ReactTooltip
-            id="tooltip--market-fees"
-            className={TooltipStyles.Tooltip}
-            effect="solid"
-            place="bottom"
-            type="light"
-          >
-            <h4>Trading Settlement Fee</h4>
-            <p>
-              The trading settlement fee is a combination of the Market Creator
-              Fee (<b>{property.marketCreatorFee}</b>) and the Reporting Fee (
-              <b>{property.reportingFee}</b>)
-            </p>
-          </ReactTooltip>
+          <div className={Styles.CoreProperties__row}>
+            <div className={Styles.CoreProperties__property}>
+              <span className={Styles[`CoreProperties__property-name`]}>
+                <div>Denominated In</div>
+              </span>
+              <span className={Styles[`CoreProperties__property-value`]}>
+                {getValue(market, "scalarDenomination")}
+              </span>
+            </div>
+          </div>
+          <div className={Styles.CoreProperties__row}>
+            <div className={Styles.CoreProperties__property__container}>
+              <div className={Styles.CoreProperties__min__max__dots}>
+                <div>
+                  <span className={Styles.CoreProperties__property__min__max}>
+                    <div>Min</div>
+                  </span>
+                  <span>{getValue(market, "minPrice").toString()}</span>
+                </div>
+                <span className={Styles.CoreProperties__dotted__line} />
+                <div>
+                  <span className={Styles.CoreProperties__property__min__max}>
+                    <div>Max</div>
+                  </span>
+                  <span>{getValue(market, "maxPrice").toString()}</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       )}
-    </span>
-    <span
-      className={Styles[`CoreProperties__property-value`]}
-      style={property.textStyle}
-    >
-      {property.value}
-      {property.denomination && (
-        <span className={Styles[`CoreProperties__property-denomination`]}>
-          {property.denomination}
-        </span>
-      )}
-    </span>
+    </div>
   </div>
 );
-
-Property.propTypes = {
-  numRow: PropTypes.number.isRequired,
-  property: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    value: PropTypes.any,
-    tooltip: PropTypes.bool,
-    textStyle: PropTypes.object,
-    marketCreatorFee: PropTypes.string,
-    reportingFee: PropTypes.string
-  }).isRequired
-};
-
-const CoreProperties = ({ market, isMobileSmall }) => {
-  const { marketType } = market;
-
-  const marketCreatorFee = getValue(market, "marketCreatorFeeRatePercent.full");
-  const reportingFee = getValue(market, "reportingFeeRatePercent.full");
-
-  const isScalar = marketType === SCALAR;
-
-  const propertyRows = [
-    [
-      {
-        name: "Total Volume",
-        value: getValue(market, "volume.formatted"),
-        denomination: "ETH",
-        textStyle: { fontSize: "18px" }
-      },
-      {
-        name: "Open Interest",
-        value: getValue(market, "openInterest.formatted"),
-        denomination: "ETH",
-        textStyle: { fontSize: "18px" }
-      }
-    ],
-    [
-      {
-        name: "24hr Volume",
-        value: "---",
-        denomination: "ETH"
-      },
-      {
-        name: "Fee",
-        value: getValue(market, "settlementFeePercent.full"),
-        tooltip: true,
-        marketCreatorFee,
-        reportingFee
-      }
-    ],
-    [
-      {
-        name: "Type",
-        value:
-          getValue(market, "marketType") === YES_NO
-            ? "Yes/No"
-            : getValue(market, "marketType")
-      }
-    ],
-    [
-      {
-        name: "denominated in",
-        value: getValue(market, "scalarDenomination"),
-        textStyle: { textTransform: "none" }
-      }
-    ]
-  ];
-
-  if (isMobileSmall && isScalar) {
-    propertyRows.push([
-      {
-        name: "min",
-        value: getValue(market, "minPrice").toString()
-      },
-      {
-        name: "max",
-        value: getValue(market, "maxPrice").toString()
-      }
-    ]);
-  }
-
-  const renderedProperties = [];
-  propertyRows.forEach((propertyRow, numRow) => {
-    const row = [];
-    propertyRow.forEach((property, numCol) => {
-      if (property.value) {
-        row.push(
-          <Property
-            key={`property${numRow}${numCol}`}
-            property={property}
-            numRow={numRow}
-          />
-        );
-      }
-    });
-    renderedProperties.push(
-      <div
-        key={`row${numRow}`}
-        className={classNames(Styles.CoreProperties__row)}
-      >
-        {row}
-      </div>
-    );
-  });
-
-  return (
-    <div className={Styles.CoreProperties__coreContainer}>
-      {renderedProperties}
-    </div>
-  );
-};
 
 CoreProperties.propTypes = {
   market: PropTypes.object.isRequired,
