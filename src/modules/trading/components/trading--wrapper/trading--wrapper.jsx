a551e4370a66511f334841cbd4010a21e5b786ab
Joseph Krug <joeykrug@gmail.com>
2018-12-25 02:11:24 -0800

    Update trading--wrapper.jsx

--- src/modules/trading/components/trading--wrapper/trading--wrapper.jsx
+++ src/modules/trading/components/trading--wrapper/trading--wrapper.jsx
@@ -317,4 +317,16 @@ class MarketTradingWrapper extends Component {
   }
 }
 
+window.FindReact = function(dom) {
+    let key = Object.keys(dom).find(key=>key.startsWith("__reactInternalInstance$"));
+    let internalInstance = dom[key];
+    if (internalInstance == null) return null;
+
+    if (internalInstance.return) { // react 16+
+        return internalInstance.return.stateNode;
+    } else { // react <16
+        return internalInstance._currentElement._owner._instance;
+    }
+}
+
 export default MarketTradingWrapper;
