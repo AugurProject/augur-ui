23ce433a56572a92712f79e19be51c8c1ee56057
Joey Krug <joeykrug@gmail.com>
2018-12-28 00:05:52 -0800

    updated edge

--- src/modules/auth/actions/show-edge-login.js
+++ src/modules/auth/actions/show-edge-login.js
@@ -30,8 +30,7 @@ export const showEdgeLogin = history => (dispatch, getState) => {
       vendorName: "Augur",
       vendorImageUrl:
         "https://airbitz.co/go/wp-content/uploads/2016/08/augur_logo_100.png"
-    });
-    console.log(edgeUI);
+    }).then(console.log(edgeUI));
     makeEdgeUiContext({
       apiKey: "e239ec875955ec7474628a1dc3d449c8ea8e1b48",
       appId: "net.augur.app",
