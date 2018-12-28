5928efe41475185180feb64e440b3a5e4d2bd0e6
Joey Krug <joeykrug@gmail.com>
2018-12-27 23:32:30 -0800

    updated edge

--- src/modules/auth/actions/show-edge-login.js
+++ src/modules/auth/actions/show-edge-login.js
@@ -17,10 +17,10 @@ export const showEdgeLogin = history => (dispatch, getState) => {
   const state = getState();
   const edgeContext = selectEdgeContextState(state);
   const edgeLoading = selectEdgeLoadingState(state);
-  edgeContext.on('login', edgeAccount => dispatch(loginWithEdge(edgeAccount, history)));
 
   if (edgeContext) {
-    edgeContext.openLoginWindow(loginWindowOptions);
+    edgeContext.on('login', edgeAccount => dispatch(loginWithEdge(edgeAccount, history)))
+    edgeContext.showLoginWindow();
   } else if (!edgeLoading) {
     dispatch(updateAuthStatus(EDGE_LOADING, true));
     makeEdgeUiContext({
@@ -31,6 +31,7 @@ export const showEdgeLogin = history => (dispatch, getState) => {
       vendorImageUrl:
         "https://airbitz.co/go/wp-content/uploads/2016/08/augur_logo_100.png"
     }).then(edgeContext => {
+      edgeContext.on('login', edgeAccount => dispatch(loginWithEdge(edgeAccount, history)));
       dispatch(updateAuthStatus(EDGE_LOADING, false));
       dispatch(updateAuthStatus(EDGE_CONTEXT, edgeContext));
       edgeContext.showLoginWindow();
