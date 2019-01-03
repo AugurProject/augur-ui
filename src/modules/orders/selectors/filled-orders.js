0f76d952fb45f238f56cbd95ef939e5725ffa741
phoebemirman <phoebe.mirman@gmail.com>
2019-01-03 14:18:51 -0800

    add filled orders selector

--- src/modules/orders/selectors/filled-orders.js
+++ src/modules/orders/selectors/filled-orders.js
@@ -1,3 +1,5 @@
+import { createBigNumber } from "utils/create-big-number";
+
 export function selectFilledOrders(marketTradeHistory, accountId) {
 	if (!marketTradeHistory || marketTradeHistory.length < 1) {
 		return [];
@@ -6,8 +8,38 @@ export function selectFilledOrders(marketTradeHistory, accountId) {
 	const filledOrders = marketTradeHistory.filter(
 	  trade => trade.creator === accountId
 	);
-	
-	filledOrders.sort((a, b) => b.timestamp - a.timestamp);
+	 // need to group by order id
+	 // [
+	 // 	{
+	 // 		quantity: 1,
+	 // 		outcome: name
+	 // 		trades: [
+	 // 			{trade}
+	 // 		]
+	 // 	}
+	 // ]
 
-	return filledOrders;
+	const orders = filledOrders.reduce((order, { orderId, outcome, amount, price, type, timestamp, transactionHash }) => {
+	  const foundOrder = order.find(({ id }) => id === orderId);
+	  if (foundOrder) {
+	  	foundOrder.trades.push({ outcome, amount, price, type, timestamp, transactionHash });
+	  	foundOrder.trades.sort((a, b) => b.timestamp - a.timestamp);
+	  	foundOrder.timestamp = foundOrder.trades[0].timestamp;
+	  	foundOrder.quantity = createBigNumber(foundOrder.quantity).plus(amount).toString(); // need to do bignumber
+	  }
+	  else order.push({ 
+	  	id: orderId, 
+	  	timestamp: timestamp,
+	  	outcome: outcome,
+	  	type: type,
+	  	price: price,
+	  	quantity: amount,
+	  	trades: [{ outcome, amount, price, type, timestamp, transactionHash }] });
+	  return order;
+	}, []);
+
+
+	orders.sort((a, b) => b.timestamp - a.timestamp);
+
+	return orders;
 }
