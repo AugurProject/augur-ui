36995bdd30c62089546b96bf9e8b408ccd131997
Joey Krug <joeykrug@gmail.com>
2019-01-04 14:28:09 -0800

    escaped apostrophe

--- src/modules/create-market/components/create-market-form-resolution/create-market-form-resolution.jsx
+++ src/modules/create-market/components/create-market-form-resolution/create-market-form-resolution.jsx
@@ -354,7 +354,14 @@ export default class CreateMarketResolution extends Component {
             </div>
           </label>
           <br />
-            <p> The market expiration date needs to be a date and time sufficiently *after* the event is over where people can publicly view the result at that time, not before or during. If an event happens on 01/01 and is reported on 01/02, the expiration date should be after the time it's reported on 01/02. </p>
+          <p>
+            {" "}
+            The market expiration date needs to be a date and time sufficiently
+            *after* the event is over where people can publicly view the result
+            at that time, not before or during. If an event happens on 01/01 and
+            is reported on 01/02, the expiration date should be after the time
+            it&apos;s reported on 01/02.{" "}
+          </p>
           <br />
           <SingleDatePicker
             id="cm__input--date"
