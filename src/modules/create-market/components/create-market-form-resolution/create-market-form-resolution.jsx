9e329dfc0685ac7263f295bc26c35f1cc446e27a
Tom Haile <bradford.haile@gmail.com>
2019-01-07 16:23:02 -0600

    linting

--- src/modules/create-market/components/create-market-form-resolution/create-market-form-resolution.jsx
+++ src/modules/create-market/components/create-market-form-resolution/create-market-form-resolution.jsx
@@ -356,9 +356,12 @@ export default class CreateMarketResolution extends Component {
           <br />
           <p>
             {" "}
-            This should be a date &amp; time sufficiently *after* the event, when the result can be viewed publicly.
+            This should be a date &amp; time sufficiently *after* the event,
+            when the result can be viewed publicly.
             <br />
-            If an event happens on 01/01 and is reported on 01/02, the expiration date should be after the time it&apos;s reported on 01/02.{" "}
+            If an event happens on 01/01 and is reported on 01/02, the
+            expiration date should be after the time it&apos;s reported on
+            01/02.{" "}
           </p>
           <br />
           <SingleDatePicker
