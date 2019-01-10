43bc0a6d7a9afa64a24dec50d3c892e0d3330e7b
Joey Krug <joeykrug@gmail.com>
2019-01-09 19:29:36 -0800

    linted

--- src/web-workers-exit.js
+++ src/web-workers-exit.js
@@ -1,15 +1,15 @@
-document.documentElement.className += (("ontouchstart" in document.documentElement) ? " touch-enabled" : " touch-disabled");
-  if (window.navigator && navigator.serviceWorker) {
-    navigator.serviceWorker.getRegistrations()
-      .then(function(registrations) {
-        if (registrations.length === 0) return;
-        var p = registrations.map(function(registration) {
-          return registration.unregister();
-        });
+document.documentElement.className +=
+  "ontouchstart" in document.documentElement
+    ? " touch-enabled"
+    : " touch-disabled";
+if (window.navigator && navigator.serviceWorker) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    if (registrations.length === 0) return;
+    const p = registrations.map(registration => registration.unregister());
 
-        return Promise.all(p).then(function() {
-          // We must reload to prevent unregistered service workers from running.
-          window.location.reload();
-        });
-      });
-  }
+    return Promise.all(p).then(() => {
+      // We must reload to prevent unregistered service workers from running.
+      window.location.reload();
+    });
+  });
+}
