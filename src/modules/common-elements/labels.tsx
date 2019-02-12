a49948db0d5c98e71c68a54d5e337832abb491ab
John Danz <JohnDanz@users.noreply.github.com>
2019-02-12 14:45:26 -0600

    Ui refactoring efforts round 3 - some Labels (#2669)
    * inital TS setup for modules, added PrimaryButton, refactored a number of ad-hoc buttons into PrimaryButtons
    
    * Fix typescript handling in tests
    
    * renamed common-elements/primary-button.tsx to .../buttons.tsx. Removed all constant files in favor of a large constants file in common-elements/constants.ts
    
    * added OrderButtons, removed unused styling in trading-wrapper
    
    * removed utils/constants
    
    * added FavoritesButton, removed nolonger needed icons, replaced favorites toggles with new button
    
    * added marketStatus and marketType labels to common-elements

---
+++ src/modules/common-elements/labels.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import classNames from "classnames";
+import * as constants from "./constants";
+import Styles from "./labels.styles";
+
+export interface MarketTypeProps {
+  marketType: string
+}
+
+export interface MarketStatusProps {
+  marketStatus: string,
+  mini?: boolean
+}
+
+export const MarketTypeLabel = (props: MarketTypeProps) => 
+  <span
+    className={Styles.MarketTypeLabel}
+  >
+    {props.marketType === constants.YES_NO ? "Yes/No" : props.marketType}
+  </span>;
+
+export const MarketStatusLabel = (props: MarketStatusProps) => {
+  const { marketStatus, mini } = props;
+  let open: boolean = false;
+  let resolved: boolean = false;
+  let reporting: boolean = false;
+  let text: string;
+  switch (marketStatus) {
+    case constants.MARKET_OPEN:
+      open = true;
+      text = constants.MARKET_STATUS_MESSAGES.OPEN;
+      break;
+    case constants.MARKET_CLOSED:
+      resolved = true;
+      text = constants.MARKET_STATUS_MESSAGES.RESOLVED;
+      break;
+    default:
+      reporting = true;
+      text = constants.MARKET_STATUS_MESSAGES.IN_REPORTING;
+      break;
+  }
+  return (
+  <span
+    className={classNames(Styles.MarketStatus, {
+      [Styles.MarketStatus_mini]: mini,
+      [Styles.MarketStatus_open]: open,
+      [Styles.MarketStatus_resolved]: resolved,
+      [Styles.MarketStatus_reporting]: reporting
+    })}
+  >
+    {text}
+  </span>
+  );
+};
