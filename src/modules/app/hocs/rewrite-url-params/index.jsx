d067c89c07d3a4583ba6ad00c6f1f6d651ec5375
Andrew D'Amelio <dev@andrewdamel.io>
2019-02-16 19:15:14 -0500

    import only required keys

--- src/modules/app/hocs/rewrite-url-params/index.jsx
+++ src/modules/app/hocs/rewrite-url-params/index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import parseQuery from "modules/routes/helpers/parse-query";
-import * as keys from "modules/common-elements/constants";
+import {
+  AUGUR_NODE_URL,
+  ETHEREUM_NODE_HTTP,
+  ETHEREUM_NODE_WS
+} from "src/modules/common-elements/constants";
 
 import { createPath } from "history";
 
 import { camelCase, compose, isEmpty, mapKeys, omit, pick } from "lodash/fp";
 import makeQuery from "src/modules/routes/helpers/make-query";
+
+const keys = {
+  AUGUR_NODE_URL,
+  ETHEREUM_NODE_HTTP,
+  ETHEREUM_NODE_WS
+};
 
 const valuesToGet = Object.values(keys);
 const grabParams = pick(valuesToGet);
