62e1e47f6f1e5b4afcc0a9f5ea7b74795602b366
Tom Haile <bradford.haile@gmail.com>
2019-02-21 14:16:59 -0600

    added linearproperty label to be sued in order form confirm and for portfolio quad components

--- src/modules/portfolio/components/common/market-row.tsx
+++ src/modules/portfolio/components/common/market-row.tsx
@@ -4,7 +4,7 @@ import classNames from "classnames";
 
 import ToggleRow from "modules/portfolio/components/common/toggle-row.tsx";
 import { MarketStatusLabel } from "modules/common-elements/labels";
-
+import { LinearPropertyLabel } from "modules/common-elements/labels";
 import Styles from "modules/portfolio/components/common/market-row.styles";
 
 // todo: MarketRow__time will end up being a passed in prop
@@ -12,24 +12,31 @@ import Styles from "modules/portfolio/components/common/market-row.styles";
 // maybe will have a boolean for whether it is extendable because the watchlist one is different
 
 export interface TimeObject {
-  formattedShortDate: string
+  formattedShortDate: string;
+}
+export interface FormatObject {
+  formatted: string;
 }
 
 export interface Market {
-  marketId: string,
-  description: string,
-  marketStatus: string,
-  creationTime: TimeObject
+  marketId: string;
+  description: string;
+  marketStatus: string;
+  creationTime: TimeObject;
+  volume: FormatObject;
+  openInterest: FormatObject;
 }
 
 export interface MarketRowProps {
-  market: Market,
-  showState: boolean
+  market: Market;
+  showState: boolean;
 }
 
 const MarketRow = (props: MarketRowProps) => (
   <ToggleRow
-    className={classNames(Styles.MarketRow, {[Styles.MarketRow__showState]: props.showState})}
+    className={classNames(Styles.MarketRow, {
+      [Styles.MarketRow__showState]: props.showState
+    })}
     topRowContent={
       props.showState && (
         <div className={Styles.MarketRow__firstRow}>
@@ -42,16 +49,28 @@ const MarketRow = (props: MarketRowProps) => (
       )
     }
     rowContent={
-      <div className={Styles.MarketRow__infoContainer}>
-        <span className={Styles.MarketRow__description}>{props.market.description}</span>
-        <span className={Styles.MarketRow__time}>{props.market.creationTime.formattedShortDate}</span>
+      <div className={Styles.MarketRow__rowContainer}>
+        <span className={Styles.MarketRow__description}>
+          {props.market.description}
+        </span>
+        <span className={Styles.MarketRow__time}>
+          {props.market.creationTime.formattedShortDate}
+        </span>
       </div>
     }
     toggleContent={
-      <div>
-        info
+      <div className={Styles.MarketRow__infoContainer}>
+          <LinearPropertyLabel
+            label="Volume"
+            value={`${props.market.volume.formatted} ETH`}
+          />
+          <LinearPropertyLabel
+            label="Open Interest"
+            value={`${props.market.openInterest.formatted} ETH`}
+          />
       </div>
     }
-/>);
+  />
+);
 
 export default MarketRow;
