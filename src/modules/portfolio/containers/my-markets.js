ad52534d639cce42f9945371d1c6285e39dca2f6
Tom Haile <bradford.haile@gmail.com>
2019-02-22 13:35:40 -0600

    added my market timer widget and needed props

--- src/modules/portfolio/containers/my-markets.js
+++ src/modules/portfolio/containers/my-markets.js
@@ -4,9 +4,7 @@ import { withRouter } from "react-router-dom";
 import MyMarkets from "modules/portfolio/components/markets/markets";
 import getUserMarkets from "modules/markets/selectors/user-markets";
 import { toggleFavorite } from "modules/markets/actions/update-favorites";
-import { loadUserMarkets } from "modules/markets/actions/load-markets";
 import {
-  loadUnclaimedFees,
   collectMarketCreatorFees
 } from "modules/markets/actions/market-creator-fees-management";
 import {
@@ -31,27 +29,15 @@ const mapStateToProps = state => {
     isMobile: state.appStatus.isMobile,
     pendingLiquidityOrders: state.pendingLiquidityOrders,
     outcomes: marketDisputeOutcomes() || {},
-    tabsInfo: createTabsInfo(markets)
+    tabsInfo: createTabsInfo(markets),
+    currentAugurTimestamp: state.blockchain.currentAugurTimestamp,
+    reportingWindowStatsEndTime: state.reportingWindowStats.endTime
   };
 };
 
 const mapDispatchToProps = dispatch => ({
-  loadMarkets: () =>
-    dispatch(
-      loadUserMarkets((err, marketIds) => {
-        if (err) return logError(err);
-        // if we have marketIds back, let's load the info so that we can properly display myMarkets.
-        dispatch(loadMarketsInfoIfNotLoaded(marketIds));
-        dispatch(loadUnclaimedFees(marketIds));
-      })
-    ),
   collectMarketCreatorFees: (getBalanceOnly, marketId, callback) =>
-    dispatch(collectMarketCreatorFees(getBalanceOnly, marketId, callback)),
-  loadMarketsInfo: marketIds => dispatch(loadMarketsInfo(marketIds)),
-  toggleFavorite: marketId => dispatch(toggleFavorite(marketId)),
-  loadMarketsInfoIfNotLoaded: marketIds =>
-    dispatch(loadMarketsInfoIfNotLoaded(marketIds)),
-  loadDisputingMarkets: () => dispatch(loadDisputing())
+    dispatch(collectMarketCreatorFees(getBalanceOnly, marketId, callback))
 });
 
 const MyMarketsContainer = withRouter(
