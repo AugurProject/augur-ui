d860e1bafec5b12c09aa829c49ccd1cadb4d50ab
Tom Haile <bradford.haile@gmail.com>
2019-03-04 17:41:28 -0600

    consolidated pendingOrders appending to userOpenOrders to market selector

--- src/modules/portfolio/containers/open-orders.js
+++ src/modules/portfolio/containers/open-orders.js
@@ -1,48 +1,21 @@
 import { connect } from "react-redux";
-import memoize from "memoizee";
 
 import OpenOrders from "modules/portfolio/components/orders/open-orders";
 import { triggerTransactionsExport } from "modules/transactions/actions/trigger-transactions-export";
 import { updateModal } from "modules/modal/actions/update-modal";
 import getOpenOrders from "modules/orders/selectors/open-orders";
 import * as constants from "src/modules/common-elements/constants";
-import { selectPendingOrdersState } from "src/select-state";
-import { selectMarket } from "modules/markets/selectors/market";
 
 const mapStateToProps = state => {
   const openOrders = getOpenOrders();
-  const markets = getPositionsMarkets(openOrders).filter(
+  const markets = openOrders.filter(
     market => market.marketStatus !== constants.MARKET_CLOSED
   );
-  const pendingOrders = selectPendingOrdersState(state);
 
-  let individualOrders = markets.reduce(
+  const individualOrders = markets.reduce(
     (p, market) => [...p, ...market.userOpenOrders],
     []
   );
-
-  Object.keys(pendingOrders).forEach(marketId => {
-    individualOrders = (pendingOrders[marketId] || []).concat(individualOrders);
-  });
-
-  Object.keys(pendingOrders).forEach(marketId => {
-    const findIndex = markets.findIndex(market => market.id === marketId);
-    if (findIndex >= 0) {
-      // market is already in the open orders list
-      markets[findIndex] = {
-        ...markets[findIndex],
-        userOpenOrders: markets[findIndex].userOpenOrders.concat(
-          pendingOrders[marketId] || []
-        )
-      };
-    } else {
-      const market = selectMarket(marketId);
-      markets.push({
-        ...market,
-        userOpenOrders: pendingOrders[marketId]
-      });
-    }
-  });
 
   const marketsObj = markets.reduce((obj, market) => {
     obj[market.id] = market;
@@ -69,11 +42,6 @@ const mapDispatchToProps = dispatch => ({
       updateModal({ type: constants.MODAL_CLAIM_TRADING_PROCEEDS, marketId })
     )
 });
-
-const getPositionsMarkets = memoize(
-  openOrders => Array.from(new Set([...openOrders])),
-  { max: 1 }
-);
 
 const OpenOrdersContainer = connect(
   mapStateToProps,
