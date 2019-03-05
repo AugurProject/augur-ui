319e78423a1fddc0e048cc0057fbdc718ba8c5b3
John Danz <JohnDanz@users.noreply.github.com>
2019-03-05 18:58:06 -0500

    added updated approval/gasselection modals (#2719)

---
+++ src/modules/modal/common.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { XIcon } from "modules/common-elements/icons";
+import { DefaultButtonProps, PrimaryButton, SecondaryButton } from "modules/common-elements/buttons";
+import Styles from "modules/modal/modal.styles";
+
+interface ModalTitleProps {
+  title: string;
+  closeAction: Function;
+};
+
+interface ModalDescriptionProps {
+  description: Array<string>;
+}
+
+interface ModalButtonsRowProps {
+  buttons: Array<DefaultButtonProps>;
+}
+
+interface ModalAlertMessageProps {
+  preText: string;
+  boldText?: string;
+  postText?: string;
+};
+
+interface SelectableTableRow {
+  columns: Array<string | number>;
+  action: Function;
+};
+
+interface ModalSelectableTableProps {
+  tableData: Array<SelectableTableRow>;
+};
+
+export const ModalTitle = (props: ModalTitleProps) => 
+  <>
+    <h1>{props.title}</h1>
+    <button onClick={() => props.closeAction()}>{XIcon}</button>
+  </>;
+
+export const ModalDescription = (props: ModalDescriptionProps) => {
+  return props.description.map(
+    (descriptionText: string) => (
+        <p key={descriptionText.slice(20).replace(" ", "-")}>
+          {descriptionText}
+        </p>
+      )
+    )
+  };
+
+export const ModalButtonsRow = (props: ModalButtonsRowProps) =>
+  (<div>
+    {props.buttons.map((Button: DefaultButtonProps, index: number) => {
+      if (index === 0) return (<PrimaryButton key={Button.text} {...Button} />)
+      return (<SecondaryButton key={Button.text} {...Button} />)
+    })}
+  </div>);
+
+export const ModalAlertMessage = (props: ModalAlertMessageProps) =>
+  (<div className={Styles.AlertMessage}>
+    {props.preText}{props.boldText && <b>&nbsp;{props.boldText}&nbsp;</b>}{props.postText}
+  </div>);
+
+export const ModalSelectableTable = (props: ModalSelectableTableProps) => 
+  (<div className={Styles.SelectableModalTable}>
+    {props.tableData.map((row: SelectableTableRow) => (
+      <button key={`row_${row.columns[0]}`} onClick={() => row.action()}>
+        {row.columns.map((columnText: string | number) => (<span key={`col_${columnText}`}>{columnText}</span>))}
+      </button>
+    ))}
+  </div>);
---
+++ src/modules/modal/containers/modal-approval.ts
@@ -0,0 +1,48 @@
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+// import ModalConfirm from "modules/modal/message-modal";
+import { MessageModal } from "modules/modal/message-modal";
+
+import { closeModal } from "modules/modal/actions/close-modal";
+import { approveAccount } from "modules/auth/actions/approve-account";
+
+const mapStateToProps = state => ({
+  modal: state.modal
+});
+
+const mapDispatchToProps = dispatch => ({
+  closeModal: () => dispatch(closeModal()),
+  approveAccount: (onSent, onSuccess) =>
+    dispatch(approveAccount(onSent, onSuccess))
+});
+
+const mergeProps = (sP, dP, oP) => {
+  return {
+    title: "Approve Augur",
+    description: [
+      `In order to trade on Augur you must first approve the Augur Contracts to move Ether on your behalf. You will be unable to trade until approval has completed.`,
+      `After clicking "Approve" you will be asked to sign a transaction, followed by a second transaction to complete your requested trade.`
+    ],
+    closeAction: () => {
+      sP.modal.approveCallback("close_modal");
+      dP.closeModal();
+    },
+    buttons: [
+      {
+        text: "Approve",
+        action: () => {
+          dP.approveAccount(sP.modal.approveOnSent, sP.modal.approveCallback);
+          dP.closeModal();
+        }
+      }
+    ]
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+    mergeProps
+  )(MessageModal)
+);
