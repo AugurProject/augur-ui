f3f4dd13b73b18970f461f87773a044a5b8e4d52
phoebemirman <phoebe.mirman@gmail.com>
2019-03-12 11:07:48 -0700

    move complete sets to own component

--- src/modules/portfolio/components/common/tables/market-positions-table.tsx
+++ src/modules/portfolio/components/common/tables/market-positions-table.tsx
@@ -4,9 +4,8 @@ import PropTypes from "prop-types";
 import PositionsHeader from "modules/portfolio/components/common/headers/positions-header";
 import PositionRow from "modules/portfolio/components/common/rows/position-row";
 import { Market, Position } from "modules/portfolio/types";
-import { AWAITING_SIGNATURE, PENDING } from "modules/common-elements/constants";
-import { CompactButton } from "modules/common-elements/buttons";
 import classNames from "classnames";
+import CompleteSets from "modules/market/components/complete-sets/complete-sets";
 
 import SharedStyles from "modules/market/components/market-orders-positions-table/open-orders-table.style";
 import Styles from "modules/portfolio/components/common/tables/market-positions-table.styles";
@@ -47,23 +46,6 @@ export class MarketPositionsTable extends React.Component<MarketPositionsTablePr
     } = this.props;
     const { showPercent } = this.state;
 
-    const pendingCompleteSetsHash = `pending-${marketId}-${numCompleteSets &&
-      numCompleteSets.fullPrecision}`;
-    const pendingCompleteSetsInfo = transactionsStatus[pendingCompleteSetsHash];
-    const status = pendingCompleteSetsInfo && pendingCompleteSetsInfo.status;
-    let completeSetButtonText = "Sell Complete Sets";
-    switch (status) {
-      case AWAITING_SIGNATURE:
-        completeSetButtonText = "Awaiting Signature...";
-        break;
-      case PENDING:
-        completeSetButtonText = "Pending transaction...";
-        break;
-      default:
-        completeSetButtonText = "Sell Complete Sets";
-        break;
-    }
-
 
     return (
       <div className={classNames(Styles.MarketPositionsTable, {[SharedStyles.MarketOpenOrdersList__table]: extendedView})}>
@@ -83,21 +65,7 @@ export class MarketPositionsTable extends React.Component<MarketPositionsTablePr
         {extendedView && positions.length === 0 && (
           <div className={SharedStyles.MarketOpenOrdersList__empty} />
         )}
-        {extendedView && numCompleteSets &&
-            numCompleteSets.value > 0 && (
-              <div className={Styles.MarketPositionsList__completeSets}>
-                <span>{`You currently have ${
-                  numCompleteSets.full
-                } of all outcomes.`}</span>
-                <CompactButton
-                  disabled={!!pendingCompleteSetsInfo}
-                  text={completeSetButtonText}
-                  action={e => {
-                    sellCompleteSets(marketId, numCompleteSets, () => {});
-                  }}
-                />
-              </div>
-            )}
+        {extendedView && <CompleteSets marketId={marketId} numCompleteSets={numCompleteSets} transactionsStatus={transactionsStatus} sellCompleteSets={sellCompleteSets} />}
       </div>
     );
   }
