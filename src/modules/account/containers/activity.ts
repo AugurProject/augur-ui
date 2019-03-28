c15f7ca9dada35f72b79236eee06c1c1c405e5eb
Tom Haile <bradford.haile@gmail.com>
2019-03-28 16:16:31 -0500

    updated to newest verion of platform stats

--- src/modules/account/containers/activity.ts
+++ src/modules/account/containers/activity.ts
@@ -1,12 +1,21 @@
 import { connect } from "react-redux";
-
+import { formatAttoEth } from "utils/format-number";
 import Activity from "modules/account/components/augur-status/activity";
 import { updatePlatformTimeframeData } from "modules/account/actions/update-platform-timeframe-data";
 import { selectCurrentTimestampInSeconds } from "src/select-state";
 
-const mapStateToProps = (state: any) => ({
-  currentAugurTimestamp: selectCurrentTimestampInSeconds(state)
-});
+const mapStateToProps = (state: any) => {
+  const value =
+    (state.universe &&
+      state.universe.timeframeData &&
+      state.universe.timeframeData.openInterest) ||
+    0;
+  const openInterest = formatAttoEth(value, { decimals: 4 });
+  return {
+    openInterest,
+    currentAugurTimestamp: selectCurrentTimestampInSeconds(state)
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
   updatePlatformTimeframeData: startTime =>
