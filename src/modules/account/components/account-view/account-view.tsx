b1c024fa4f98f607da6088845e87511eefc0db7b
phoebemirman <phoebe.mirman@gmail.com>
2019-03-28 11:32:47 -0700

    layout for account mobile page

--- src/modules/account/components/account-view/account-view.tsx
+++ src/modules/account/components/account-view/account-view.tsx
@@ -7,26 +7,58 @@ import AugurStatus from "modules/account/containers/augur-status";
 import Favorites from "modules/portfolio/containers/favorites";
 import OpenMarkets from "modules/account/containers/open-markets";
 import AccountOverview from "modules/account/containers/account-overview";
+import ModuleTabs from "modules/market/components/common/module-tabs/module-tabs";
+import ModulePane from "modules/market/components/common/module-tabs/module-pane";
 
 import Styles from "modules/account/components/account-view/account-view.styles";
 
-const AccountView = () => (
+export interface AccountViewProps {
+  isMobile?: Boolean;
+}
+
+const AccountView = (props: AccountViewProps) => (
   <section className={Styles.AccountView}>
-    <div className={Styles.AccountView__container}>
-      <div>
-        <Notifications />
-        <OpenMarkets />
-      </div>
-      <div>
-        <AccountOverview />
-        <AugurStatus />
-      </div>
-      <div>
-        <Favorites />
-        <TransactionsBoxContainer />
-      </div>
-    </div>
-    <TermsAndConditions />
+    {props.isMobile && (
+      <ModuleTabs selected={0} fillWidth noBorder>
+        <ModulePane label="Your Overview">
+          <AccountOverview />
+        </ModulePane>
+        <ModulePane label="Notifications">
+          <Notifications />
+        </ModulePane>
+        <ModulePane label="Watchlist">
+          <Favorites />
+        </ModulePane>
+        <ModulePane label="Open Markets">
+          <OpenMarkets />
+        </ModulePane>
+        <ModulePane label="Augur Status">
+          <AugurStatus />
+        </ModulePane>
+        <ModulePane label="Transactions">
+          <TransactionsBoxContainer />
+        </ModulePane>
+      </ModuleTabs>
+    )}
+    {!props.isMobile && (
+      <>
+        <div className={Styles.AccountView__container}>
+          <div>
+            <Notifications />
+            <OpenMarkets />
+          </div>
+          <div>
+            <AccountOverview />
+            <AugurStatus />
+          </div>
+          <div>
+            <Favorites />
+            <TransactionsBoxContainer />
+          </div>
+        </div>
+        <TermsAndConditions />
+      </>
+    )}
   </section>
 );
 
