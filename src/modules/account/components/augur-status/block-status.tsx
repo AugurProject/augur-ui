e2909680f94ae7bc71ae5c0cb631365220699fc7
phoebemirman <phoebe.mirman@gmail.com>
2019-03-28 16:08:29 -0700

    augur status mobile

--- src/modules/account/components/augur-status/block-status.tsx
+++ src/modules/account/components/augur-status/block-status.tsx
@@ -3,6 +3,7 @@ import classNames from "classnames";
 
 import { formatNumber } from "utils/format-number";
 import * as constants from "modules/common-elements/constants";
+import { LinearPropertyLabel } from "modules/common-elements/labels";
 
 import Styles from "modules/account/components/augur-status/block-status.styles";
 
@@ -16,22 +17,35 @@ const BlockStatus = (props: BlockStatusProps) => {
   const { blocksBehind, lastProcessedBlockBn, highestBlockBn } = props;
   return (
     <>
-      <div className={Styles.BlockStatus}>
-          <div>
-            <div>{constants.SYNC_BENIND}</div>
-            <div>{blocksBehind}</div>
-          </div>
+      <div className={classNames(Styles.BlockStatus, Styles.HideOnMobile)}>
+        <div>
+          <div>{constants.SYNC_BENIND}</div>
+          <div>{blocksBehind}</div>
+        </div>
 
+        <div>
+          <div>{constants.SYNC_PROCESSED}</div>
           <div>
-            <div>{constants.SYNC_PROCESSED}</div>
-            <div>
-              {formatNumber(lastProcessedBlockBn.toString()).formatted} /{" "}
-              {formatNumber(highestBlockBn.toString()).formatted}
-            </div>
+            {formatNumber(lastProcessedBlockBn.toString()).formatted} /{" "}
+            {formatNumber(highestBlockBn.toString()).formatted}
           </div>
         </div>
       </div>
       <div className={Styles.ShowOnMobile}>
+        <LinearPropertyLabel
+          highlight
+          label={constants.SYNC_BENIND}
+          value={blocksBehind}
+        />
+        <LinearPropertyLabel
+          highlight
+          label={constants.SYNC_PROCESSED}
+          value={
+            formatNumber(lastProcessedBlockBn.toString()).formatted +
+            "/" +
+            formatNumber(highestBlockBn.toString()).formatted
+          }
+        />
       </div>
     </>
   );
