bb805b186f6d110f07c35b8fcb531a80f2348d87
phoebemirman <phoebe.mirman@gmail.com>
2019-04-08 14:07:03 -0700

    add fee window claimable to list

--- src/modules/modal/containers/modal-claim-fees.js
+++ src/modules/modal/containers/modal-claim-fees.js
@@ -3,13 +3,9 @@ import { withRouter } from "react-router-dom";
 
 import { selectMarket } from "modules/markets/selectors/market";
 import { CLAIM_SHARES_GAS_COST } from "modules/positions/actions/claim-trading-proceeds";
-import { createBigNumber } from "utils/create-big-number";
+// import { createBigNumber } from "utils/create-big-number";
 import { getGasPrice } from "modules/auth/selectors/get-gas-price";
-import {
-  formatGasCostToEther,
-  formatEther,
-  formatAttoRep
-} from "utils/format-number";
+import { formatGasCostToEther, formatAttoRep } from "utils/format-number";
 import { closeModal } from "modules/modal/actions/close-modal";
 import { Proceeds } from "modules/modal/proceeds";
 import { ActionRowsProps } from "modules/modal/common";
@@ -26,6 +22,7 @@ const mapStateToProps = (state: any) => ({
     getGasPrice(state)
   ),
   reportingFees: state.reportingWindowStats.reportingFees,
+  feeWindows: state.reportingWindowStats.reportingFees.feeWindows,
   nonforkedMarkets: state.reportingWindowStats.reportingFees.nonforkedMarkets
 });
 
@@ -39,45 +36,43 @@ const mapDispatchToProps = (dispatch: Function) => ({
 const mergeProps = (sP: any, dP: any, oP: any) => {
   const marketIdsToTest = sP.nonforkedMarkets;
   const markets: Array<ActionRowsProps> = [];
-  const marketIds: Array<string> = [];
   marketIdsToTest.forEach(marketObj => {
     const market = selectMarket(marketObj.marketId);
     if (market) {
       markets.push({
         title: market.description,
-        text: "Claim Stake & Fees",
+        text: "Claim Proceeds",
         properties: [
           {
             label: "reporting stake",
-            value: marketObj.unclaimedRepTotal
-              ? formatAttoRep(marketObj.unclaimedRepTotal, {
-                  decimals: 4,
-                  decimalsRounded: 4,
-                  zeroStyled: true
-                })
-              : 0
+            value: `${
+              formatAttoRep(marketObj.unclaimedRepTotal || 0, {
+                decimals: 4,
+                decimalsRounded: 4,
+                zeroStyled: true
+              }).formatted
+            } REP`
           },
           {
             label: "Reporting Fees",
-            value: 0
+            value: `${0} ETH`
           },
           {
             label: "est gas cost",
-            value: 0
+            value: `${0} ETH`
           },
           {
             label: "total",
-            value: 0
+            value: `${0} ETH`
           }
         ],
         action: () => {
           const market = sP.reportingFees.nonforkedMarkets.find(
             market => market.marketId === marketObj.marketId
           );
-          console.log(market);
           const ClaimReportingFeesNonforkedMarketsOptions = {
             feeWindows: [], // fee windows is empty here because we are just claiming by markets
-            forkedMarket: sP.reportingFees.forkedMarket,
+            forkedMarket: [],
             nonforkedMarkets: [market],
             estimateGas: false,
             onSent: () => {},
@@ -92,12 +87,52 @@ const mergeProps = (sP: any, dP: any, oP: any) => {
           );
         }
       });
-      marketIds.push(marketObj.marketId);
     }
   });
-  const totalGas = formatEther(
-    createBigNumber(sP.gasCost).times(markets.length)
-  );
+  if (sP.feeWindows.length > 0) {
+    markets.push({
+      title: "Fees from all fee windows",
+      text: "Claim Fees",
+      properties: [
+        {
+          label: "reporting stake",
+          value: `${sP.reportingFees.participationTokenRepStaked.formatted} REP`
+        },
+        {
+          label: "Reporting Fees",
+          value: `${
+            sP.reportingFees.unclaimedParticipationTokenEthFees.formatted
+          } ETH`
+        },
+        {
+          label: "est gas cost",
+          value: `${0} ETH`
+        },
+        {
+          label: "total",
+          value: `${0} ETH`
+        }
+      ],
+      action: () => {
+        const ClaimReportingFeesNonforkedMarketsOptions = {
+          feeWindows: sP.feeWindows,
+          forkedMarket: [],
+          nonforkedMarkets: [],
+          estimateGas: false,
+          onSent: () => {},
+          onSuccess: result => {
+            if (sP.modal.cb) {
+              sP.modal.cb();
+            }
+          }
+        };
+        dP.claimReportingFeesNonforkedMarkets(
+          ClaimReportingFeesNonforkedMarketsOptions
+        );
+      }
+    });
+  }
+
   return {
     title: "Claim Stake & Fees",
     descriptionMessage: [
@@ -116,19 +151,19 @@ const mergeProps = (sP: any, dP: any, oP: any) => {
     breakdown: [
       {
         label: "Reporting Stake",
-        value: sP.reportingFees.unclaimedRep.full
+        value: `${sP.reportingFees.unclaimedRep.full} REP`
       },
       {
         label: "Reporting Fees",
-        value: sP.reportingFees.unclaimedEth.full
+        value: `${sP.reportingFees.unclaimedEth.full} ETH`
       },
       {
         label: "Estimated Gas",
-        value: totalGas.full
+        value: `${0} ETH`
       },
       {
         label: "Total",
-        value: 0
+        value: `${0} ETH`
       }
     ],
     closeAction: () => {
@@ -142,14 +177,12 @@ const mergeProps = (sP: any, dP: any, oP: any) => {
         text: "Claim All Stake & Fees",
         action: () => {
           const reportingParticipants = [];
-          sP.nonforkedMarkets.forEach(
-            nonforkedMarket => {
-              reportingParticipants.push(nonforkedMarket.initialReporter);
-              nonforkedMarket.crowdsourcers.forEach(crowdsourcer => {
-                reportingParticipants.push(crowdsourcer);
-              });
-            }
-          );
+          sP.nonforkedMarkets.forEach(nonforkedMarket => {
+            reportingParticipants.push(nonforkedMarket.initialReporter);
+            nonforkedMarket.crowdsourcers.forEach(crowdsourcer => {
+              reportingParticipants.push(crowdsourcer);
+            });
+          });
           const ClaimReportingFeesNonforkedMarketsOptions = {
             _feeWindows: sP.reportingFees.feeWindows,
             _reportingParticipants: reportingParticipants,
