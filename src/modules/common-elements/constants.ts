42b97d2ef90751f5817447894d11f523e7b98b61
Tom Haile <bradford.haile@gmail.com>
2019-04-11 12:01:16 -0500

    setting period interval to get more curves in the profit and loss chart

--- src/modules/common-elements/constants.ts
+++ src/modules/common-elements/constants.ts
@@ -659,7 +659,7 @@ export const TIMEFRAME_OPTIONS = [
   { label: TIMEFRAMES.DAY, periodInterval: 86400, id: 0 },
   { label: TIMEFRAMES.WEEK, periodInterval: 604800, id: 1 },
   { label: TIMEFRAMES.MONTH, periodInterval: 2592000, id: 2 },
-  { label: TIMEFRAMES.ALL, periodInterval: 0, id: 3 }
+  { label: TIMEFRAMES.ALL, periodInterval: 86400, id: 3 }
 ];
 
 // Pending Queue Types
