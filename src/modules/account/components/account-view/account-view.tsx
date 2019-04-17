5fbe02526db10ac6c951419d6e6ca5005256508d
phoebemirman <phoebe.mirman@gmail.com>
2019-04-17 15:41:12 -0700

    account pg responsive breakpoints

--- src/modules/account/components/account-view/account-view.tsx
+++ src/modules/account/components/account-view/account-view.tsx
@@ -11,7 +11,6 @@ import AccountOverview from "modules/account/containers/account-overview";
 import ModuleTabs from "modules/market/components/common/module-tabs/module-tabs";
 import ModulePane from "modules/market/components/common/module-tabs/module-pane";
 import * as constants from "modules/common-elements/constants";
-import { SMALL_MOBILE } from "modules/common-elements/constants";
 
 import Styles from "modules/account/components/account-view/account-view.styles";
 
@@ -21,44 +20,101 @@ export interface AccountViewProps {
 }
 
 const AccountView = (props: AccountViewProps) => (
-   <Media query={SMALL_MOBILE}>
-    {matches =>
-      matches ? (
-         <ModuleTabs selected={0} fillWidth noBorder>
-          <ModulePane label={constants.YOUR_OVERVIEW_TITLE}>
-            <AccountOverview />
-          </ModulePane>
-          <ModulePane label="Notifications" isNew={props.newNotifications}>
-            <Notifications />
-          </ModulePane>
-          <ModulePane label="Watchlist">
-            <Favorites />
-          </ModulePane>
-          <ModulePane label="My Active Markets">
-            <OpenMarkets />
-          </ModulePane>
-          <ModulePane label={constants.AUGUR_STATUS_TITLE}>
-            <AugurStatus />
-          </ModulePane>
-          <ModulePane label="Transactions">
-            <TransactionsBoxContainer />
-          </ModulePane>
-        </ModuleTabs>
-      ) : (
-        <section className={Styles.AccountView}>
-          <div className={Styles.AccountView__container}>
-            <Notifications />
-            <AccountOverview />
-            <Favorites />
-            <OpenMarkets />
-            <AugurStatus />
-            <TransactionsBoxContainer />
-          </div>
-          <TermsAndConditions />
-        </section>
-      )
-    }
-  </Media>
+  <>
+    <Media query={constants.SMALL_MOBILE}>
+      {matches =>
+        matches && (
+          <ModuleTabs selected={0} fillWidth noBorder>
+            <ModulePane label={constants.YOUR_OVERVIEW_TITLE}>
+              <AccountOverview />
+            </ModulePane>
+            <ModulePane label="Notifications" isNew={props.newNotifications}>
+              <Notifications />
+            </ModulePane>
+            <ModulePane label="Watchlist">
+              <Favorites />
+            </ModulePane>
+            <ModulePane label="My Active Markets">
+              <OpenMarkets />
+            </ModulePane>
+            <ModulePane label={constants.AUGUR_STATUS_TITLE}>
+              <AugurStatus />
+            </ModulePane>
+            <ModulePane label="Transactions">
+              <TransactionsBoxContainer />
+            </ModulePane>
+          </ModuleTabs>
+        )
+      }
+    </Media>
+    <Media query={constants.TABLET}>
+      {matches =>
+        matches && (
+          <section className={Styles.AccountView}>
+            <div className={Styles.AccountView__container}>
+              <div>
+                <Notifications />
+                <OpenMarkets />
+                <AugurStatus />
+              </div>
+              <div>
+                <AccountOverview />
+                <Favorites />
+                <TransactionsBoxContainer />
+              </div>
+            </div>
+            <TermsAndConditions />
+          </section>
+        )
+      }
+    </Media>
+    <Media query={constants.DESKTOP}>
+      {matches =>
+        matches && (
+          <section className={Styles.AccountView}>
+            <div className={Styles.AccountView__container}>
+              <div>
+                <Notifications />
+                <OpenMarkets />
+              </div>
+              <div>
+                <AccountOverview />
+                <AugurStatus />
+              </div>
+              <div>
+                <Favorites />
+                <TransactionsBoxContainer />
+              </div>
+            </div>
+            <TermsAndConditions />
+          </section>
+        )
+      }
+    </Media>
+    <Media query={constants.LARGE_DESKTOP}>
+      {matches =>
+        matches && (
+          <section className={Styles.AccountView}>
+            <div className={Styles.AccountView__container}>
+              <div>
+                <Notifications />
+                <AugurStatus />
+              </div>
+              <div>
+                <AccountOverview />
+                <TransactionsBoxContainer />
+              </div>
+              <div>
+                <Favorites />
+                <OpenMarkets />
+              </div>
+            </div>
+            <TermsAndConditions />
+          </section>
+        )
+      }
+    </Media>
+  </>
 );
 
 export default AccountView;
--- src/modules/common-elements/constants.ts
+++ src/modules/common-elements/constants.ts
@@ -670,4 +670,7 @@ export const CLAIM_PROCEEDS = "CLAIM_PROCEEDS";
 export const CLAIM_FEE_WINDOWS = "CLAIM_FEE_WINDOWS";
 
 // Media Queries
-export const SMALL_MOBILE = "(max-width: 768px)";
+export const SMALL_MOBILE = "(max-width: 768px)"; // matches @breakpoint-mobile-mid
+export const TABLET = "(min-width: 768px) and (max-width: 1200px)";
+export const DESKTOP = "(min-width:1201px) and (max-width: 2000px)";
+export const LARGE_DESKTOP = "(min-width: 2001px)";
