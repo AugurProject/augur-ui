fc672a0717da76107b1b08d0fa769e805985e0a0
phoebemirman <phoebe.mirman@gmail.com>
2019-05-03 14:52:02 -0700

    make cut off readable a constant

--- src/modules/markets/constants/cutoff-date.js
+++ src/modules/markets/constants/cutoff-date.js
@@ -1,5 +1,7 @@
-// End Date 
+import { formatDate } from "utils/format-date";
 
-// todo: update this 
+// Cutoff Date
+// update this
 // Tuesday, May 3, 2022 9:09:52 PM
 export const CUTOFF = 1651612192;
+export const CUTOFF_READABLE = formatDate(new Date(CUTOFF * 1000)).formattedUtc;
