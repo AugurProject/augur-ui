014e4da9cd3656564b7c8ff1822bed84dcf2e63d
phoebemirman <phoebe.mirman@gmail.com>
2019-05-07 14:08:55 -0700

    add banners for market trading page, and report/dispute

--- src/modules/app/components/inner-banner/inner-banner.jsx
+++ src/modules/app/components/inner-banner/inner-banner.jsx
@@ -2,19 +2,40 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { CUTOFF_READABLE } from "modules/markets/constants/cutoff-date";
-import { AlertIcon } from "modules/common/components/icons";
-import { CREATE_MARKET } from "modules/routes/constants/views";
+import { AlertIcon, Flag } from "modules/common/components/icons";
+import {
+  CREATE_MARKET,
+  MARKET,
+  REPORT,
+  DISPUTE
+} from "modules/routes/constants/views";
 
 import Styles from "modules/app/components/inner-banner/inner-banner.styles";
 
 const InnerBanner = ({ currentPath }) => (
   <section className={Styles.InnerBanner}>
-    <div>{currentPath === CREATE_MARKET && AlertIcon}</div>
+    <div>{currentPath === CREATE_MARKET ? AlertIcon : Flag}</div>
     {currentPath === CREATE_MARKET && (
       <span>
         Due to a planned Augur v2 launch, no new markets can be created that end
         after {CUTOFF_READABLE}. <br />
         Any markets that end after this date cannot be guaranteed to resolve
+        correctly.
+      </span>
+    )}
+    {(currentPath === DISPUTE || currentPath === REPORT) && (
+      <span>
+        Users are advised <b>not to report on or dispute</b> this market because
+        it expires after {CUTOFF_READABLE} for the Augur v2 release phase.
+        <br />
+        Doing so may put your REP at risk of getting stuck in Augur v1
+      </span>
+    )}
+    {currentPath === MARKET && (
+      <span>
+        This market expires after the cutoff date for the Augur v2 release
+        phase. <br />
+        Markets ending after {CUTOFF_READABLE} cannot be guaranteed to resolve
         correctly.
       </span>
     )}
