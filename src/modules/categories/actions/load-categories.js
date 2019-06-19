1d3667a0e59620927896915857a5fb8b4c0249fb
Tom Haile <bradford.haile@gmail.com>
2019-06-19 17:01:49 -0500

    simplify call to getMarkets with filtering params and call load categories with same filtering params

--- src/modules/categories/actions/load-categories.js
+++ src/modules/categories/actions/load-categories.js
@@ -5,28 +5,34 @@ import {
 } from "modules/categories/actions/update-categories";
 import logError from "utils/log-error";
 
-const loadCategories = (callback = logError) => (dispatch, getState) => {
+const loadCategories = (params = {}, callback = logError) => (
+  dispatch,
+  getState
+) => {
   const { universe } = getState();
   if (!universe.id) return callback(null);
-  augur.markets.getCategories({ universe: universe.id }, (err, categories) => {
-    if (err) return callback(err);
-    if (categories == null) return callback(null);
-    dispatch(clearCategories());
+  augur.markets.getCategories(
+    { ...params, universe: universe.id },
+    (err, categories) => {
+      if (err) return callback(err);
+      if (categories == null) return callback(null);
+      dispatch(clearCategories());
 
-    // Categories currently don't allow the user to specify how they should be
-    // sorted; to enable users to discover interesting markets, sort categories
-    // and their nested tags by non-finalized open interest. Do this here
-    // because this sort is expensive and we only want to do it once, upstream
-    // of redux store. Possibly this could be done in augur-node instead,
-    // such that the UI would just rely on categories already being sorted.
-    categories.sort(sortByNonFinalizedOpenInterestDescending);
-    categories.forEach(c =>
-      c.tags.sort(sortByNonFinalizedOpenInterestDescending)
-    );
+      // Categories currently don't allow the user to specify how they should be
+      // sorted; to enable users to discover interesting markets, sort categories
+      // and their nested tags by non-finalized open interest. Do this here
+      // because this sort is expensive and we only want to do it once, upstream
+      // of redux store. Possibly this could be done in augur-node instead,
+      // such that the UI would just rely on categories already being sorted.
+      categories.sort(sortByNonFinalizedOpenInterestDescending);
+      categories.forEach(c =>
+        c.tags.sort(sortByNonFinalizedOpenInterestDescending)
+      );
 
-    dispatch(updateCategories(categories));
-    callback(null, categories);
-  });
+      dispatch(updateCategories(categories));
+      callback(null, categories);
+    }
+  );
 };
 
 export default loadCategories;
