12309e758f3970c14c28e6711ec0479305d166eb
Peter Hicks <petong@jah.net>
2019-10-27 09:49:40 -0700

    6.9.3 (#3127)
    * point mainnet at cloudlfare
    
    * 6.9.3

--- src/modules/app/constants/endpoint-url-params.js
+++ src/modules/app/constants/endpoint-url-params.js
@@ -10,4 +10,4 @@ export const ETHEREUM_NODE_WS = "ethereum_node_ws";
 // hostedFallbacks
 export const HOSTED_AUGUR_NODE = "wss://augur-node.augur.casino";
 export const HOSTED_ETHEREUM_NODE =
-  "https://eth-mainnet.alchemyapi.io/jsonrpc/7sE1TzCIRIQA3NJPD5wg7YRiVjhxuWAE";
+  "https://cloudflare-eth.com/";
