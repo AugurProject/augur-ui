fab23a671e8d4fd7b942c03b1108fc4f320baa88
Paul Gebheim <pgebheim@gmail.com>
2019-10-31 00:03:04 +0000

    Push cutoff date by 1 day

--- src/modules/markets/constants/cutoff-date.js
+++ src/modules/markets/constants/cutoff-date.js
@@ -1,9 +1,10 @@
 import { formatDate } from "utils/format-date";
 
 // Cutoff Date
-// Wednesday, January 1, 2020 12:00:00 AM
+// Thursday, January 2, 2020 12:00:00 AM
+// old Wednesday, January 1, 2020 12:00:00 AM
 // old cutoff date Sunday, September 15, 2019 11:59:59 PM
 
-export const CUTOFF = 1577836800000;
+export const CUTOFF = 1577923200000;
 export const CUTOFF_READABLE = formatDate(new Date(CUTOFF)).formattedUtc;
 export const CUTOFF_URL = "https://www.augur.net/blog/v1-cutoff-update/";
